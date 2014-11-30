2d69dca288029c170ec05928c62fbe4e3d65127b
Marek Trtik <mtrtik@gmail.com>
2014-11-30 17:11:59 +0100

    * the code of a test of an efloop is written, but it was not properly tested.

--- code/tests/efloop_construction_and_simulation/my_environment.cpp
+++ code/tests/efloop_construction_and_simulation/my_environment.cpp
@@ -1,5 +1,49 @@
 #include "./my_environment.hpp"
-#include <utility/development.hpp>
+#include "./my_cell.hpp"
+#include "./my_synapse.hpp"
+#include "./my_signalling.hpp"
+#include <cellab/utilities_for_transition_algorithms.hpp>
+#include <utility/random.hpp>
+#include <utility/test.hpp>
+#include <boost/chrono.hpp>
+#include <vector>
+#include <thread>
+
+static void wait_milliseconds(natural_32_bit const num_millisecond)
+{
+    double const  max_duration = num_millisecond / 1000.0;
+    for (boost::chrono::system_clock::time_point const  start = boost::chrono::system_clock::now();
+         boost::chrono::duration<double>(boost::chrono::system_clock::now() - start).count() < max_duration;
+         )
+    {}
+}
+
+
+static void  thread_update_sensory_cells(
+        efloop::access_to_sensory_cells const&  access_to_sensory_cells,
+        efloop::access_to_synapses_to_muscles const&  access_to_synapses_to_muscles,
+        natural_32_bit  cell_index,
+        natural_32_bit const  shift_to_next_cell
+        )
+{
+    wait_milliseconds(get_random_natural_32_bit_in_range(0U,1000U));
+
+    for ( ; cell_index < access_to_sensory_cells.num_sensory_cells(); cell_index += shift_to_next_cell)
+    {
+        instance_wrapper<my_cell> cell;
+        access_to_sensory_cells.read_sensory_cell(cell_index,cell);
+        cell->increment();
+
+        for (natural_32_bit i = 0, n = get_random_natural_32_bit_in_range(0U,10U); i < n; ++i)
+        {
+            instance_wrapper<my_synapse> synapse;
+            access_to_synapses_to_muscles.read_synapse_to_muscle(
+                        get_random_natural_32_bit_in_range(0U,access_to_synapses_to_muscles.num_synapses_to_muscles()-1U),
+                        synapse);
+            TEST_SUCCESS(synapse->count() == cell->count());
+        }
+    }
+}
 
 
 void my_environment::compute_next_state(
@@ -8,5 +52,23 @@ void my_environment::compute_next_state(
         natural_32_bit const  num_threads_avalilable_for_computation
         )
 {
-    NOT_IMPLEMENTED_YET();
+    std::vector<std::thread> threads;
+    for (natural_32_bit i = 1U; i < num_threads_avalilable_for_computation; ++i)
+        threads.push_back(
+                    std::thread(
+                        &thread_update_sensory_cells,
+                        std::cref(access_to_sensory_cells),
+                        std::cref(access_to_synapses_to_muscles),
+                        i,
+                        num_threads_avalilable_for_computation
+                        )
+                    );
+    thread_update_sensory_cells(
+                access_to_sensory_cells,
+                access_to_synapses_to_muscles,
+                0U,
+                num_threads_avalilable_for_computation
+                );
+    for(std::thread& thread : threads)
+        thread.join();
 }
--- code/tests/efloop_construction_and_simulation/my_neural_tissue.cpp
+++ code/tests/efloop_construction_and_simulation/my_neural_tissue.cpp
@@ -1,32 +1,34 @@
 #include "./my_neural_tissue.hpp"
 #include <utility/development.hpp>
+#include <utility/random.hpp>
+#include <utility/test.hpp>
 #include <vector>
 
 
 natural_16_bit  num_kinds_of_tissue_cells() noexcept { return 3U; }
-natural_16_bit  num_kinds_of_sensory_cells() noexcept { return 2U; }
+natural_16_bit  num_kinds_of_sensory_cells() noexcept { return 5U; }
 
 natural_16_bit  num_bits_per_cell() noexcept { return 8U * sizeof(my_cell); }
 natural_16_bit  num_bits_per_synapse() noexcept { return 8U * sizeof(my_synapse); }
 natural_16_bit  num_bits_per_signalling() noexcept { return 8U * sizeof(my_signalling); }
 
-natural_32_bit  num_cells_along_x_axis() noexcept { return 100U; }
-natural_32_bit  num_cells_along_y_axis() noexcept { return 50U; }
+natural_32_bit  num_cells_along_x_axis() noexcept { return 31U; }
+natural_32_bit  num_cells_along_y_axis() noexcept { return 32U; }
 
 std::vector<natural_32_bit> const&  num_tissue_cells_of_cell_kind() noexcept {
     static std::vector<natural_32_bit> v = {7U,5U,3U};
     return v;
     }
 std::vector<natural_32_bit> const&  num_synapses_in_territory_of_cell_kind() noexcept {
-    static std::vector<natural_32_bit> v = {20U,10U,5U};
+    static std::vector<natural_32_bit> v = {5U,4U,3U};
     return v;
     }
 std::vector<natural_32_bit> const&  num_sensory_cells_of_cell_kind() noexcept {
-    static std::vector<natural_32_bit> v = {50U,30U};
+    static std::vector<natural_32_bit> v = {500U,400U,100U,600U,1400U};
     return v;
     }
 
-natural_32_bit  num_synapses_to_muscles() noexcept { return 100U; }
+natural_32_bit  num_synapses_to_muscles() noexcept { return 3000U; }
 
 bool  is_x_axis_torus_axis() noexcept { return true; }
 bool  is_y_axis_torus_axis() noexcept { return false; }
@@ -104,24 +106,92 @@ my_neural_tissue::my_neural_tissue()
           columnar_radius_of_cellular_neighbourhood_of_signalling(),
           get_automated_binding_of_transition_functions()
           )
-{}
+{
+    for (natural_32_bit x = 0U; x < get_static_state_of_neural_tissue()->num_cells_along_x_axis(); ++x)
+        for (natural_32_bit y = 0U; y < get_static_state_of_neural_tissue()->num_cells_along_y_axis(); ++y)
+            for (natural_32_bit c = 0U; c < get_static_state_of_neural_tissue()->num_cells_along_columnar_axis(); ++c)
+            {
+                value_to_bits(0U,get_dynamic_state_of_neural_tissue()->find_bits_of_cell_in_tissue(x,y,c));
+                value_to_bits(0U,get_dynamic_state_of_neural_tissue()->find_bits_of_signalling(x,y,c));
+
+                natural_32_bit const num_synapses =
+                        get_static_state_of_neural_tissue()->num_synapses_in_territory_of_cell_kind(
+                                get_static_state_of_neural_tissue()->compute_kind_of_cell_from_its_position_along_columnar_axis(c)
+                                );
+                for (natural_32_bit s = 0U; s < num_synapses; ++s)
+                {
+                    value_to_bits(0U,get_dynamic_state_of_neural_tissue()->find_bits_of_synapse_in_tissue(x,y,c,s));
+
+                    cellab::territorial_state_of_synapse const  territorial_state =
+                            (cellab::territorial_state_of_synapse)get_random_natural_32_bit_in_range(
+                                    cellab::territorial_state_of_synapse::SIGNAL_DELIVERY_TO_CELL_OF_TERRITORY,
+                                    cellab::territorial_state_of_synapse::MIGRATION_ALONG_NEGATIVE_COLUMNAR_AXIS
+                                    );
+                    value_to_bits(territorial_state,get_dynamic_state_of_neural_tissue()->find_bits_of_territorial_state_of_synapse_in_tissue(x,y,c,s));
+
+                    bits_reference bits_of_coords =
+                            get_dynamic_state_of_neural_tissue()->find_bits_of_coords_of_source_cell_of_synapse_in_tissue(x,y,c,s);
+                    natural_16_bit const num_bits = get_dynamic_state_of_neural_tissue()->num_bits_per_source_cell_coordinate();
+                    value_to_bits(
+                            get_random_natural_32_bit_in_range(0U,get_static_state_of_neural_tissue()->num_cells_along_x_axis()-1U),
+                            bits_of_coords,0U,num_bits);
+                    value_to_bits(
+                            get_random_natural_32_bit_in_range(0U,get_static_state_of_neural_tissue()->num_cells_along_y_axis()-1U),
+                            bits_of_coords,num_bits,num_bits);
+                    value_to_bits(
+                            get_random_natural_32_bit_in_range(0U,get_static_state_of_neural_tissue()->num_cells_along_columnar_axis()+
+                                                                  get_static_state_of_neural_tissue()->num_sensory_cells()-1U),
+                            bits_of_coords,num_bits+num_bits,num_bits);
+
+                    natural_8_bit const d0 =
+                        cellab::convert_territorial_state_of_synapse_to_territorial_list_index(territorial_state);
+                    for (natural_8_bit d = 0U; d < cellab::num_delimiters(); ++d)
+                        if (d < d0)
+                            value_to_bits(0U,get_dynamic_state_of_neural_tissue()->find_bits_of_delimiter_between_teritorial_lists(x,y,c,d));
+                        else
+                            value_to_bits(num_synapses,get_dynamic_state_of_neural_tissue()->find_bits_of_delimiter_between_teritorial_lists(x,y,c,d));
+                }
+            }
+
+    for (natural_32_bit i = 0U; i < get_static_state_of_neural_tissue()->num_synapses_to_muscles(); ++i)
+    {
+        value_to_bits(0U,get_dynamic_state_of_neural_tissue()->find_bits_of_synapse_to_muscle(i));
+
+        bits_reference bits_of_coords =
+                get_dynamic_state_of_neural_tissue()->find_bits_of_coords_of_source_cell_of_synapse_to_muscle(i);
+        natural_16_bit const num_bits = get_dynamic_state_of_neural_tissue()->num_bits_per_source_cell_coordinate();
+        value_to_bits(
+                get_random_natural_32_bit_in_range(0U,get_static_state_of_neural_tissue()->num_cells_along_x_axis()-1U),
+                bits_of_coords,0U,num_bits);
+        value_to_bits(
+                get_random_natural_32_bit_in_range(0U,get_static_state_of_neural_tissue()->num_cells_along_y_axis()-1U),
+                bits_of_coords,num_bits,num_bits);
+        value_to_bits(
+                get_random_natural_32_bit_in_range(0U,get_static_state_of_neural_tissue()->num_cells_along_columnar_axis()-1U),
+                bits_of_coords,num_bits+num_bits,num_bits);
+    }
+
+    for (natural_32_bit i = 0U; i < get_static_state_of_neural_tissue()->num_sensory_cells(); ++i)
+        value_to_bits(0U,get_dynamic_state_of_neural_tissue()->find_bits_of_sensory_cell(i));
+}
 
 void  my_neural_tissue::transition_function_of_synapse_to_muscle(
         my_synapse& synapse_to_be_updated,
-        cellab::kind_of_cell const kind_of_source_cell,
+        cellab::kind_of_cell,
         my_cell const& source_cell
         )
 {
-    NOT_IMPLEMENTED_YET();
+    synapse_to_be_updated.increment();
+    TEST_SUCCESS(synapse_to_be_updated.count() == source_cell.count() + 1U);
 }
 
 cellab::territorial_state_of_synapse  my_neural_tissue::transition_function_of_synapse_inside_tissue(
         my_synapse& synapse_to_be_updated,
-        cellab::kind_of_cell const kind_of_source_cell,
+        cellab::kind_of_cell,
         my_cell const& source_cell,
-        cellab::kind_of_cell const kind_of_territory_cell,
+        cellab::kind_of_cell,
         my_cell const& territory_cell,
-        cellab::territorial_state_of_synapse const current_territorial_state_of_synapse,
+        cellab::territorial_state_of_synapse,
         cellab::shift_in_coordinates const& shift_to_low_corner,
         cellab::shift_in_coordinates const& shift_to_high_corner,
         std::function<cellab::kind_of_cell(cellab::shift_in_coordinates const&,
@@ -129,13 +199,26 @@ cellab::territorial_state_of_synapse  my_neural_tissue::transition_function_of_s
             get_signalling
         )
 {
-    NOT_IMPLEMENTED_YET();
-    //return current_territorial_state_of_synapse;
+    synapse_to_be_updated.increment();
+
+    TEST_SUCCESS(synapse_to_be_updated.count() == source_cell.count() + 1U);
+    TEST_SUCCESS(synapse_to_be_updated.count() == territory_cell.count() + 1U);
+
+    instance_wrapper<my_signalling const> signalling;
+    get_signalling(shift_to_low_corner,signalling);
+    TEST_SUCCESS(synapse_to_be_updated.count() == signalling->count() + 1U);
+    get_signalling(shift_to_high_corner,signalling);
+    TEST_SUCCESS(synapse_to_be_updated.count() == signalling->count() + 1U);
+
+    return (cellab::territorial_state_of_synapse)get_random_natural_32_bit_in_range(
+                    cellab::territorial_state_of_synapse::SIGNAL_DELIVERY_TO_CELL_OF_TERRITORY,
+                    cellab::territorial_state_of_synapse::MIGRATION_ALONG_NEGATIVE_COLUMNAR_AXIS
+                    );
 }
 
 void my_neural_tissue::transition_function_of_signalling(
         my_signalling& signalling_to_be_updated,
-        cellab::kind_of_cell kind_of_territory_cell,
+        cellab::kind_of_cell,
         cellab::shift_in_coordinates const& shift_to_low_corner,
         cellab::shift_in_coordinates const& shift_to_high_corner,
         std::function<cellab::kind_of_cell(cellab::shift_in_coordinates const&,
@@ -143,12 +226,18 @@ void my_neural_tissue::transition_function_of_signalling(
             get_cell
         )
 {
-    NOT_IMPLEMENTED_YET();
+    signalling_to_be_updated.increment();
+
+    instance_wrapper<my_cell const> cell;
+    get_cell(shift_to_low_corner,cell);
+    TEST_SUCCESS(signalling_to_be_updated.count() == cell->count() + 1U);
+    get_cell(shift_to_high_corner,cell);
+    TEST_SUCCESS(signalling_to_be_updated.count() == cell->count() + 1U);
 }
 
 void my_neural_tissue::transition_function_of_cell(
         my_cell& cell_to_be_updated,
-        cellab::kind_of_cell kind_of_cell_to_be_updated,
+        cellab::kind_of_cell,
         natural_32_bit num_of_synapses_connected_to_the_cell,
         std::function<std::pair<cellab::kind_of_cell,cellab::kind_of_cell>(
                             natural_32_bit const index_of_synapse,
@@ -161,5 +250,19 @@ void my_neural_tissue::transition_function_of_cell(
             get_signalling
         )
 {
-    NOT_IMPLEMENTED_YET();
+    cell_to_be_updated.increment();
+
+    for (natural_32_bit i = 0U; i < num_of_synapses_connected_to_the_cell; ++i)
+    {
+        instance_wrapper<my_synapse const> synapse;
+        get_connected_synapse_at_index(i,synapse);
+        TEST_SUCCESS(cell_to_be_updated.count() == synapse->count());
+    }
+
+    instance_wrapper<my_signalling const> signalling;
+    get_signalling(shift_to_low_corner,signalling);
+    TEST_SUCCESS(cell_to_be_updated.count() == signalling->count());
+
+    get_signalling(shift_to_high_corner,signalling);
+    TEST_SUCCESS(cell_to_be_updated.count() == signalling->count());
 }
