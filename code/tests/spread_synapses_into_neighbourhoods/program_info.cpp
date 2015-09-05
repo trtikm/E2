464f0adcabcd6365d779e455e44aaf058c61aec8
Marek Trtik <trtikm@gmail.com>
2015-09-05 23:44:33 +0200

    * rename of the test of synapses spreading (the world 'local' is removed).

--- code/tests/spread_synapses_into_neighbourhoods/program_info.cpp
+++ code/tests/spread_synapses_into_neighbourhoods/program_info.cpp
@@ -2,7 +2,7 @@
 
 std::string  get_program_name()
 {
-    return "spread_synapses_into_local_neighbourhoods";
+    return "spread_synapses_into_neighbourhoods";
 }
 
 std::string  get_program_version()
@@ -21,10 +21,10 @@ std::string  get_program_description()
             "'fill_src_coords_of_synapses'. Then the algorithm for spreading\n"
             "synapses is called for each pair of kinds of tissue cells, for two\n"
             "pairs of xy-diameters of neighbourhoods, and for 1 and 16 available\n"
-            "threads. The work is done according to a 'distribution matrix'\n"
-            "passed to the algorithm. Then for each column and for each pair of\n"
-            "kinds of cells there is checked whether counts of synapses in all\n"
-            "columns in the neighbourhood of the checked column equals to counts\n"
-            "inside the distribution matrix."
+            "threads. Each odd/even call to the algorithm is passed identity/general\n"
+            "column shift function. Once all synapses are spread, then for each column\n"
+            "and for each pair of kinds of cells there is checked whether counts of\n"
+            "synapses in all columns in the neighbourhood of the checked column equals\n"
+            "to counts inside the distribution matrix build and passed to the algorithm."
             ;
 }
