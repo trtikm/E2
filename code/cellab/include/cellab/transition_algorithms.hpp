aad39dfe8663e4c6890b8fecd6aa7fc41f0eafb2
Marek Trtik <trtikm@gmail.com>
2016-01-16 13:36:44 +0100

    * documnetation of the transition algorithm of tissue synapses (not finished yet!)

--- code/cellab/include/cellab/transition_algorithms.hpp
+++ code/cellab/include/cellab/transition_algorithms.hpp
@@ -30,9 +30,11 @@ namespace cellab {
 
 
 /**
- * It defines a prototype of a user-defined callback function which is called for each
- * synapse to muscle from the algorithm 'apply_transition_of_synapses_to_muscles' (see bellow)
- * in order to compute a next state of the synapse to muscle from the current one.
+ * It defines a prototype of a user-defined callback function which is called for each synapse to
+ * muscle from the algorithm 'apply_transition_of_synapses_to_muscles' (see its declaration bellow)
+ * in order to compute a next state of the synapse from the current one.
+ *
+ * We assume the call-back function implement some single-threaded, thread-safe, and in-place algorithm.
  *
  * For more info read the documentation:
  *      file:///<E2-root-dir>/doc/project_documentation/cellab/cellab.html#algorithm_synapses_to_muscles
@@ -40,40 +42,72 @@ namespace cellab {
 typedef std::function<
             void(
                 bits_reference& bits_of_synapse_to_be_updated,
-                        //!< It is a reference to the the memory where the current state of the synapse
+                        //!< It is a reference to the the memory where the current state of the updated synapse
                         //!< to muscle is stored. The size, and content of this memory is defined by the
-                        //!< user. So, the user already knows how to read and interpret the content.
+                        //!< user. We thus assume that the function already has (or is build on) that information.
                         //!< The function is supposed to compute a next state from the state encoded here.
-                        //!< One the next state is computed the function have to write that state to
+                        //!< Once the next state is computed the function have to write that state to
                         //!< this memory, i.e. the function overwrites the old state by the computed one.
                 kind_of_synapse_to_muscle kind_of_synapse_to_muscle_to_be_updated,
                         //!< This information may be helpful or even necessary for the function to correctly
                         //!< interpret the content of the memory referenced by 'bits_of_synapse_to_be_updated'.
-                        //!< It can also be used for selection of a particular routine, which will actually
-                        //!< compute the next state of the considered synapse to muscle.
+                        //!< It can, for example, be used for a selection of a particular sub-routine, which will
+                        //!< actually compute the next state of the considered synapse to muscle.
                 kind_of_cell kind_of_source_cell,
                         //!< A synapse to muscle is always related to a cell in the tissue. The cell represents
-                        //!< a source of information for the synapse how about how to update its current state.
-                        //!< The kind of that cell may be important for correct understanding of data referenced
-                        //!< by the argument bellow.
+                        //!< a source of information for the synapse about how to update its current state.
                 bits_const_reference const& bits_of_source_cell
                         //!< It is a reference to the the memory where the current state of the tissue cell
-                        //!< related to the updated synapse to muscle is stored. It serves as a source of
-                        //!< information how the update the current state of the synapse to muscle to a next one.
-                        //!< The size, and content of the referenced memory is defined by the user. So, the user
-                        //!< already knows how to read and interpret the content. Nevertheless the kind of the
-                        //!< cell can be necessery, so it is passed as well by the previous parameter 'kind_of_source_cell'.
+                        //!< related to the updated synapse to muscle is stored. It serves as a source of information
+                        //!< about how to update the current state of the synapse to muscle to the next one.
+                        //!< The size, and content of the referenced memory is defined by the user. We thus assume that
+                        //!< the function already has (or is build on) that information.
                 ) >
         single_threaded_in_situ_transition_function_of_packed_dynamic_state_of_synapse_to_muscle;
 
 
+/**
+ * It defines a prototype of a user-defined callback function which is called for each synapse in the
+ * tissue from the algorithm 'apply_transition_of_synapses_of_tissue' (see its declaration bellow)
+ * in order to compute a next state of the synapse from the current one.
+ *
+ * We assume the call-back function implement some single-threaded, thread-safe, and in-place algorithm.
+ *
+ * For more info read the documentation:
+ *      file:///<E2-root-dir>/doc/project_documentation/cellab/cellab.html#algorithm_synapses_in_tissue
+ */
 typedef std::function<
             territorial_state_of_synapse(
+                        //!< The function is supposed to return a new (updated) territorial state of the
+                        //!< processed synapse (see the header file 'territorial_state_of_synapse.hpp' for
+                        //!< info about territorial states).
                 bits_reference& bits_of_synapse_to_be_updated,
+                        //!< It is a reference to the the memory where the current state of the updated synapse
+                        //!< in the tissue is stored. The size, and content of this memory is defined by the
+                        //!< user. We thus assume that the function already has (or is build on) that information.
+                        //!< The function is supposed to compute a next state from the state encoded here.
+                        //!< Once the next state is computed the function have to write that state to
+                        //!< this memory, i.e. the function overwrites the old state by the computed one.
                 kind_of_cell kind_of_source_cell,
+                        //!< A synapse in the tissue is always related to a source cell in the tissue. The cell
+                        //!< represents a source of information for the synapse about how to update its current state.
                 bits_const_reference const& bits_of_source_cell,
+                        //!< It is a reference to the the memory where the current state of the source tissue
+                        //!< cell of the updated synapse is stored. It serves as a source of information about how
+                        //!< to update the current state of the synapse to the next one. The size, and content of
+                        //!< the referenced memory is defined by a user. We thus assume that the function already
+                        //!< has (or is build on) that information.
                 kind_of_cell kind_of_territory_cell,
+                        //!< A synapse in the tissue always appears in some territory and there it can participate
+                        //!< in information delivery to the tissue cell appearing in that territory as well. This
+                        //!< parameter provides information about the kind of the target tissue cell.
                 bits_const_reference const& bits_of_territory_cell,
+                        //!< A synapse in the tissue always appears in some territory and there it can participate in
+                        //!< information delivery to the tissue cell appearing in that territory as well. This target cell
+                        //!< may thus represent a source of information for the synapse about how to update its current state.
+                        //!< This parameter is a reference to the the memory where the current state of the target tissue
+                        //!< cell of the updated synapse is stored. The size, and content of the referenced memory is defined
+                        //!< by a user. We thus assume that the function already has (or is build on) that information.
                 territorial_state_of_synapse current_territorial_state_of_synapse,
                 shift_in_coordinates const& shift_to_low_corner,
                 shift_in_coordinates const& shift_to_high_corner,
