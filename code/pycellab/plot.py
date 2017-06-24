a0091c26673fdb05294d5a6531defd1f1bd03f27
Marek Trtik <trtikm@gmail.com>
2017-06-24 19:04:21 +0100

    Added scatter plot per partes.

--- code/pycellab/plot.py
+++ code/pycellab/plot.py
@@ -80,6 +80,45 @@ def scatter(points, pathname, colours=None, title=None, xaxis_name=None, faxis_n
     __write_xplot(lambda ax, x, y: ax.scatter(x, y, s=2, c=colours), points, pathname, title, xaxis_name, faxis_name)
 
 
+def scatter_per_partes(points, pathname, start, end, step, on_plot_part_callback_fn=None,
+                       colours=None, title=None, xaxis_name=None, faxis_name=None):
+    assert start < end and step > 0.0001
+    assert len(points) > 0
+    assert on_plot_part_callback_fn is None or callable(on_plot_part_callback_fn)
+    if not os.path.exists(os.path.dirname(pathname)):
+        os.mkdir(os.path.dirname(pathname))
+    base_pathname, extension = os.path.splitext(pathname)
+    x = start
+    idx = 0
+    while x < end:
+        part_end = min(x + step, end)
+        part_points = []
+        if colours is None:
+            part_colours = None
+        else:
+            part_colours = []
+        for i, p in enumerate(points):
+            if x <= p[0] and p[0] <= part_end:
+                part_points.append(p)
+                if colours is not None:
+                    if isinstance(colours, list):
+                        part_colours.append(colours[i])
+                    else:
+                        part_colours.append(colours)
+        part_pathname = base_pathname + "_" + str(idx).zfill(4) + "_" + format(x, ".4f") + extension
+        part_title = "[part #" + str(idx) + "] "
+        if title is None:
+            part_title += "#points=" + str(len(part_points)) +\
+                          ", mean=" + format(sum([n for _, n in part_points]) / (len(part_points) + 0.000001), ".2f")
+        else:
+            part_title += title
+        if on_plot_part_callback_fn is not None:
+            on_plot_part_callback_fn(part_pathname)
+        scatter(part_points, part_pathname, part_colours, part_title, xaxis_name, faxis_name)
+        x += step
+        idx += 1
+
+
 def histogram(distrib, pathname, normalised=True, colours=None, title=None, xaxis_name=None, faxis_name=None):
     if isinstance(distrib, dict):   # 'distrib' is a plain histogram.
         distrib = distribution.distribution(distrib)
@@ -90,13 +129,14 @@ def histogram(distrib, pathname, normalised=True, colours=None, title=None, xaxi
         else:
             title = ""
         title += (
+            "#bars=" + str(len(distrib.get_histogram())) + ", "
             "sum bars=" + format(sum([value for _, value in distrib.get_histogram().items()]), ".3f") + ", "
             "median=" + format(distrib.get_median(), ".3f") + ", "
             "mean=" + format(distrib.get_mean(), ".3f") + ", "
-            "mean frequency=" + format(1.0 / (distrib.get_mean() + 0.0001), ".3f") + ", "
+            "mean freq.=" + format(1.0 / (distrib.get_mean() + 0.0001), ".3f") + ", "
             "variance=" + format(distrib.get_variance(), ".3f") + ", "
-            "standard deviation=" + format(distrib.get_standard_deviation(), ".3f") + ", "
-            "coefficient of variation=" + format(distrib.get_coefficient_of_variation(), ".3f")
+            "std. deviation=" + format(distrib.get_standard_deviation(), ".3f") + ", "
+            "CV=" + format(distrib.get_coefficient_of_variation(), ".3f")
             )
     if normalised:
         points = distrib.get_probability_points()
