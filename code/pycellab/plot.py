ec057a39e7527b956f2ba9071ad30d92f31d793f
Marek Trtik <trtikm@gmail.com>
2017-06-25 11:42:06 +0100

    Added stride to scatter plot per partes.

--- code/pycellab/plot.py
+++ code/pycellab/plot.py
@@ -80,9 +80,10 @@ def scatter(points, pathname, colours=None, title=None, xaxis_name=None, faxis_n
     __write_xplot(lambda ax, x, y: ax.scatter(x, y, s=2, c=colours), points, pathname, title, xaxis_name, faxis_name)
 
 
-def scatter_per_partes(points, pathname, start, end, step, on_plot_part_callback_fn=None,
+def scatter_per_partes(points, pathname, start, end, step, stride=1, on_plot_part_callback_fn=None,
                        colours=None, title=None, xaxis_name=None, faxis_name=None):
     assert start < end and step > 0.0001
+    assert isinstance(stride, int) and stride > 0
     assert len(points) > 0
     assert on_plot_part_callback_fn is None or callable(on_plot_part_callback_fn)
     if not os.path.exists(os.path.dirname(pathname)):
@@ -91,30 +92,31 @@ def scatter_per_partes(points, pathname, start, end, step, on_plot_part_callback
     x = start
     idx = 0
     while x < end:
-        part_end = min(x + step, end)
-        part_points = []
-        if colours is None:
-            part_colours = None
-        else:
-            part_colours = []
-        for i, p in enumerate(points):
-            if x <= p[0] and p[0] <= part_end:
-                part_points.append(p)
-                if colours is not None:
-                    if isinstance(colours, list):
-                        part_colours.append(colours[i])
-                    else:
-                        part_colours.append(colours)
-        part_pathname = base_pathname + "_" + str(idx).zfill(4) + "_" + format(x, ".4f") + extension
-        part_title = "[part #" + str(idx) + "] "
-        if title is None:
-            part_title += "#points=" + str(len(part_points)) +\
-                          ", mean=" + format(sum([n for _, n in part_points]) / (len(part_points) + 0.000001), ".2f")
-        else:
-            part_title += title
-        if on_plot_part_callback_fn is not None:
-            on_plot_part_callback_fn(part_pathname)
-        scatter(part_points, part_pathname, part_colours, part_title, xaxis_name, faxis_name)
+        if idx % stride == 0:
+            part_end = min(x + step, end)
+            part_points = []
+            if colours is None:
+                part_colours = None
+            else:
+                part_colours = []
+            for i, p in enumerate(points):
+                if x <= p[0] and p[0] <= part_end:
+                    part_points.append(p)
+                    if colours is not None:
+                        if isinstance(colours, list):
+                            part_colours.append(colours[i])
+                        else:
+                            part_colours.append(colours)
+            part_pathname = base_pathname + "_" + str(idx).zfill(4) + "_" + format(x, ".4f") + extension
+            part_title = "[part #" + str(idx) + "] "
+            if title is None:
+                part_title += "#points=" + str(len(part_points)) +\
+                              ", mean=" + format(sum([n for _, n in part_points]) / (len(part_points) + 0.000001), ".2f")
+            else:
+                part_title += title
+            if on_plot_part_callback_fn is not None:
+                on_plot_part_callback_fn(part_pathname)
+            scatter(part_points, part_pathname, part_colours, part_title, xaxis_name, faxis_name)
         x += step
         idx += 1
 
