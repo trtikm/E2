0df981802732560dd284adafe9d8099ccb2cdf92
Marek Trtik <trtikm@gmail.com>
2017-06-28 23:51:33 +0100

    Added prototypes of data signal spike trains and related tests.

--- code/pycellab/signalling.py
+++ code/pycellab/signalling.py
@@ -1,3 +1,5 @@
+import bisect
+import numpy
 import distribution
 
 
@@ -58,6 +60,41 @@ class DataSignal:
             spiking_distributions=[
                 distribution.distribution(distribution.mkhist_by_linear_interpolation([
                     # (ISI, count)
+                    (0.005, 25),
+                    (0.020, 100),
+                    (0.050, 10),
+                    ], 0.001)),
+                # distribution.distribution(distribution.mkhist_by_linear_interpolation([
+                #     # (ISI, count)
+                #     (0.010, 100),
+                #     (0.050, 10),
+                #     (0.100, 0),
+                #     ], 0.001)),
+                ],
+            selector_distribution=distribution.distribution(dict([(i, 1.0) for i in range(1)])),
+            start_time=start_time
+        )
+
+
+    @staticmethod
+    def default_inhibitory(start_time=0.0):
+        return DataSignal(
+            phase_distribution=distribution.distribution(distribution.mkhist_by_linear_interpolation([
+                # (ISI, count)
+                (0.010, 600),
+                (0.020, 800),
+                (0.030, 600),
+                (0.040, 400),
+                (0.050, 300),
+                (0.060, 200),
+                (0.070, 100),
+                (0.080, 50),
+                (0.090, 25),
+                (0.100, 10),
+                ], 0.001)),
+            spiking_distributions=[
+                distribution.distribution(distribution.mkhist_by_linear_interpolation([
+                    # (ISI, count)
                     (0.005, 10),
                     (0.010, 100),
                     ], 0.001)),
@@ -73,4 +110,151 @@ class DataSignal:
         )
 
 
+class ISIDistributionMatrix:
+    def __init__(self, events_distribution, distances_distribution):
+        assert isinstance(events_distribution, distribution.distribution)
+        assert isinstance(distances_distribution, distribution.distribution)
+        self._events_distribution = events_distribution
+        self._distances_distribution = distances_distribution
+        self._distributions_matrix = {}
+        for event0 in events_distribution.get_events():
+            hist = {}
+            for event1 in events_distribution.get_events():
+                probability = events_distribution.get_probability_of_event(event1)
+                distance = distances_distribution.get_probability_of_event(event1 - event0)
+                # hist[event1] = max(0.0, probability * distance)
+                # hist[event1] = max(0.0, probability + distance)
+                hist[event1] = max(0.0, probability * (1.0 + 10000*distance))
+            self._distributions_matrix[event0] = distribution.distribution(hist)
 
+    def get_events_distribution(self):
+        return self._events_distribution
+
+    def get_distances_distribution(self):
+        return self._distances_distribution
+
+    def get_distributions_matrix(self):
+        return self._distributions_matrix
+
+    def get_distribution_of_event(self, event):
+        assert event in self.get_distributions_matrix()
+        return self.get_distributions_matrix()[event]
+
+    @staticmethod
+    def create(frequency_coef, distance_correlation_coef):
+        assert isinstance(frequency_coef, float) and frequency_coef >= 0.0 and frequency_coef <= 1.0
+        assert isinstance(distance_correlation_coef, float) and distance_correlation_coef >= 0.0 and distance_correlation_coef <= 1.0
+        events_distribution = distribution.hermit_distribution(0.137, pow_y=2)
+        distances_distribution = distribution.hermit_distribution(0.5, pow_y=1.5, scale_x=0.2, shift_x=-0.2 / 2.0)
+        return ISIDistributionMatrix(events_distribution, distances_distribution)
+
+
+class DataSignalEx:
+    def __init__(self, distribution_matrix, start_time=0.0):
+        assert isinstance(distribution_matrix, ISIDistributionMatrix)
+        self._distribution_matrix = distribution_matrix
+        self._event_construction_time = start_time
+        self._last_event = self._distribution_matrix.get_events_distribution().next_event()
+
+    def on_time_step(self, t, dt):
+        if self._event_construction_time + self._last_event > t + dt:
+            return False
+        self._event_construction_time = t
+        self._last_event = self._distribution_matrix.get_distribution_of_event(self._last_event).next_event()
+        return True
+
+    def get_distribution_matrix(self):
+        return self._distribution_matrix
+
+
+class DataSignalFX:
+    def __init__(self, spiking_distribution, chunk_size_distribution, array_size):
+        assert isinstance(spiking_distribution, distribution.distribution)
+        assert isinstance(chunk_size_distribution, distribution.distribution)
+        assert all(isinstance(event, int) and event > 0 for event in chunk_size_distribution.get_events())
+        assert isinstance(array_size, int) and array_size >= chunk_size_distribution.get_events()[-1]
+        self._spiking_distribution = spiking_distribution
+        self._chunk_size_distribution = chunk_size_distribution
+        self._array_size = array_size
+        self._array = []
+        self._spikes_buffer = []
+
+    @staticmethod
+    def create_plain_distribution(spiking_distribution):
+        return DataSignalFX(spiking_distribution, distribution.distribution({1: 1.0}), 1)
+
+    @staticmethod
+    def create_excitatory():
+        return DataSignalFX(distribution.default_excitatory_isi_distribution(),
+                            distribution.distribution(dict([(size, 1.0) for size in range(5, 25)])),
+                            150)
+
+    def get_spiking_distribution(self):
+        return self._spiking_distribution
+
+    def get_chunk_size_distribution(self):
+        return self._chunk_size_distribution
+
+    def get_array_size(self):
+        return self._array_size
+
+    def _recharge_array(self):
+        while len(self._array) < self.get_array_size():
+            event = self.get_spiking_distribution().next_event()
+            self._array.insert(bisect.bisect_left(self._array, event), event)
+
+    def _recharge_spikes_buffer(self, t):
+        assert len(self._array) == self.get_array_size()
+        assert len(self._spikes_buffer) == 0
+        start, size = self._get_next_chunk()
+        self._copy_chunk_to_buffer(start, size)
+        self._random_shuffle_buffer()
+        self._concretise_spike_times_in_buffer(t)
+        self._remove_chunk_from_array(start, size)
+
+    def _get_next_chunk(self):
+        start = int(self.get_array_size() * numpy.random.uniform(0.0, 1.0))
+        assert start in range(self.get_array_size())
+        size = self.get_chunk_size_distribution().next_event()
+        assert size in range(1, self.get_array_size() + 1)
+        return start, size
+
+    def _copy_chunk_to_buffer(self, start, size):
+        self._spikes_buffer = [self._array[(start + i) % self.get_array_size()] for i in range(size)]
+        assert len(self._spikes_buffer) > 0
+
+    def _random_shuffle_buffer(self):
+        for _ in range(len(self._spikes_buffer)):
+            i = int(len(self._spikes_buffer) * numpy.random.uniform(0.0, 1.0))
+            j = int(len(self._spikes_buffer) * numpy.random.uniform(0.0, 1.0))
+            tmp = self._spikes_buffer[i]
+            self._spikes_buffer[i] = self._spikes_buffer[j]
+            self._spikes_buffer[j] = tmp
+
+    def _concretise_spike_times_in_buffer(self, start_time):
+        assert len(self._spikes_buffer) > 0
+        self._spikes_buffer[0] += start_time
+        for i in range(1, len(self._spikes_buffer)):
+            self._spikes_buffer[i] += self._spikes_buffer[i - 1]
+        self._spikes_buffer.reverse()
+
+    def _remove_chunk_from_array(self, start, size):
+        end = (start + size) % self.get_array_size()
+        if end >= start:
+            for _ in range(size):
+                self._array.pop(start)
+        else:
+            while len(self._array) >= start:
+                self._array.pop()
+            for _ in range(end):
+                self._array.pop(0)
+
+    def on_time_step(self, t, dt):
+        if len(self._spikes_buffer) == 0:
+            self._recharge_array()
+            self._recharge_spikes_buffer(t)
+            assert len(self._spikes_buffer) > 0
+        if self._spikes_buffer[-1] > t + dt:
+            return False
+        self._spikes_buffer.pop()
+        return True
--- code/pycellab/tests.py
+++ code/pycellab/tests.py
@@ -1,4 +1,5 @@
 import os
+import shutil
 import numpy
 from config import output_root_dir
 import plot
@@ -236,10 +237,40 @@ def _test_data_signal_default_excitatory(my_precomputed_full_name):
     """This is test 'data_signal_default_excitatory'."""
     print("Starting test '" + my_precomputed_full_name + "':")
 
+    # output_dir = os.path.join(output_root_dir(), my_precomputed_full_name)
+    # os.makedirs(output_dir, exist_ok=True)
+    #
+    # pathname = os.path.join(output_dir, "xe_isi_hist.png")
+    # print("    Saving plot " + pathname)
+    # plot.histogram(
+    #     distribution.hermit_distribution(0.137, pow_y=2),
+    #     pathname,
+    #     normalised=True
+    #     )
+    #
+    # pathname = os.path.join(output_dir, "xi_isi_hist.png")
+    # print("    Saving plot " + pathname)
+    # plot.histogram(
+    #     distribution.hermit_distribution(0.0918, 0.08, pow_y=2),
+    #     pathname,
+    #     normalised=True
+    #     )
+    #
+    # pathname = os.path.join(output_dir, "xi_counts.png")
+    # print("    Saving plot " + pathname)
+    # plot.histogram(
+    #     dict((x, (0.08 - 0.002) / x) for x in numpy.arange(0.002, 0.08, 0.001)),
+    #     pathname,
+    #     normalised=False
+    #     )
+    #
+    # print("Done.")
+    # return
+
     start_time = 0.0
     dt = 0.001
-    nsteps = 1000
-    train = spike_train.spike_train(distribution.distribution(None),
+    nsteps = 60000
+    train = spike_train.spike_train(distribution.hermit_distribution(0.25),
                                     signalling.DataSignal.default_excitatory(start_time),
                                     start_time)
     t = start_time
@@ -253,12 +284,267 @@ def _test_data_signal_default_excitatory(my_precomputed_full_name):
     output_dir = os.path.join(output_root_dir(), my_precomputed_full_name)
     os.makedirs(output_dir, exist_ok=True)
 
-    pathname = os.path.join(output_dir, "spikes_board.png")
+    pathname = os.path.join(output_dir, "isi_hist.png")
     print("    Saving plot " + pathname)
-    plot.scatter(
-        [(event, 0.0) for event in train.get_spikes()],
+    plot.histogram(
+        distribution.make_isi_histogram(
+            train.get_spikes(),
+            nsteps,
+            start_time,
+            start_time + nsteps * dt
+            ),
         pathname,
-        ['C4' if is_data_spike else 'C0' for is_data_spike in train.get_is_data_signal_flags()],
+        normalised=False
+        )
+    if False:
+        pathname = os.path.join(output_dir, "spikes_board", "spikes.png")
+        print("    Saving plot " + pathname)
+        plot.scatter_per_partes(
+            [(event, 0.0) for event in train.get_spikes()],
+            pathname,
+            start_time,
+            start_time + nsteps * dt,
+            1.0,
+            max(1, nsteps // 10),
+            lambda p: print("    Saving plot " + p),
+            ['C4' if is_data_spike else 'C0' for is_data_spike in train.get_is_data_signal_flags()],
+            )
+
+    print("Done.")
+
+
+def _test_data_signal_ex(my_precomputed_full_name):
+    """This is test 'data_signal_ex'."""
+    print("Starting test '" + my_precomputed_full_name + "':")
+
+    start_time = 0.0
+    dt = 0.001
+    nsteps = 60000
+
+    plot_dt = 1.0
+    plot_parts = 10
+    plot_stride = max(1, int((nsteps * dt / plot_dt) / plot_parts + 0.5))
+
+    data_signal = signalling.DataSignalEx(signalling.ISIDistributionMatrix.create(0.0, 1.0))
+    train = spike_train.spike_train(data_signal.get_distribution_matrix().get_events_distribution(), None, start_time)
+    spikes_ex = []
+
+    t = start_time
+    for step in range(nsteps):
+        print("    " + format(100.0 * step / float(nsteps), '.1f') + "%", end='\r')
+        train.on_time_step(t, dt)
+        if data_signal.on_time_step(t, dt):
+            spikes_ex.append(t + dt)
+        t += dt
+
+    print("  Saving results.")
+
+    output_dir = os.path.join(output_root_dir(), my_precomputed_full_name)
+    if os.path.exists(output_dir):
+        shutil.rmtree(output_dir)
+    os.makedirs(output_dir, exist_ok=True)
+
+    pathname = os.path.join(output_dir, "hist_matrix_event_dist.png")
+    print("    Saving plot " + pathname)
+    plot.histogram(
+        data_signal.get_distribution_matrix().get_events_distribution(),
+        pathname,
+        normalised=True
+        )
+
+    pathname = os.path.join(output_dir, "hist_matrix_distance_dist.png")
+    print("    Saving plot " + pathname)
+    plot.histogram(
+        data_signal.get_distribution_matrix().get_distances_distribution(),
+        pathname,
+        normalised=True
+        )
+
+    idx = 0
+    for event in sorted(data_signal.get_distribution_matrix().get_distributions_matrix().keys()):
+        distrib = data_signal.get_distribution_matrix().get_distributions_matrix()[event]
+        if idx % max(1, len(data_signal.get_distribution_matrix().get_distributions_matrix().keys()) / 10) == 0:
+            pathname = os.path.join(output_dir, "matrix_distributions",
+                                    "hist_matrix_hist_event_" + str(idx) + "_" + format(event, ".3f") + ".png")
+            print("    Saving plot " + pathname)
+            plot.histogram(
+                distrib,
+                pathname,
+                normalised=True
+                )
+        idx += 1
+
+    pathname = os.path.join(output_dir, "isi_hist.png")
+    print("    Saving plot " + pathname)
+    plot.histogram(
+        distribution.make_isi_histogram(
+            train.get_spikes(),
+            nsteps,
+            start_time,
+            start_time + nsteps * dt
+            ),
+        pathname,
+        normalised=False
+        )
+
+    pathname = os.path.join(output_dir, "isi_hist_ex.png")
+    print("    Saving plot " + pathname)
+    plot.histogram(
+        distribution.make_isi_histogram(
+            spikes_ex,
+            nsteps,
+            start_time,
+            start_time + nsteps * dt
+            ),
+        pathname,
+        normalised=False
+        )
+
+    pathname = os.path.join(output_dir, "spikes_board", "spikes.png")
+    print("    Saving plot " + pathname)
+    plot.scatter_per_partes(
+        [(event, 0.0) for event in train.get_spikes()] + [(event, 1.0) for event in spikes_ex],
+        pathname,
+        start_time,
+        start_time + nsteps * dt,
+        plot_dt,
+        plot_stride,
+        lambda p: print("    Saving plot " + p)
+        )
+
+    print("Done.")
+
+
+def _test_data_signal_fx(my_precomputed_full_name):
+    """This is test 'data_signal_fx'."""
+    print("Starting test '" + my_precomputed_full_name + "':")
+
+    start_time = 0.0
+    dt = 0.001
+    nsteps = 60000
+
+    num_spikers_per_kind = 50
+
+    plot_dt = 2.0
+    plot_parts = 10
+    plot_stride = max(1, int((nsteps * dt / plot_dt) / plot_parts + 0.5))
+
+    data_signals = [signalling.DataSignalFX.create_excitatory() for _ in range(num_spikers_per_kind)]
+    # data_signals = [signalling.DataSignalFX.create_plain_distribution(distribution.default_excitatory_isi_distribution())
+    #                 for _ in range(num_spikers_per_kind)]
+    trains = [spike_train.spike_train(data_signals[i].get_spiking_distribution(), None, start_time)
+              for i in range(num_spikers_per_kind)]
+    spikes_of_data_signals = [[] for _ in range(num_spikers_per_kind)]
+
+    t = start_time
+    for step in range(nsteps):
+        print("    " + format(100.0 * step / float(nsteps), '.1f') + "%", end='\r')
+        for i in range(num_spikers_per_kind):
+            trains[i].on_time_step(t, dt)
+            if data_signals[i].on_time_step(t, dt):
+                spikes_of_data_signals[i].append(t + dt)
+        t += dt
+
+    print("  Saving results.")
+
+    output_dir = os.path.join(output_root_dir(), my_precomputed_full_name)
+    if os.path.exists(output_dir):
+        shutil.rmtree(output_dir)
+    os.makedirs(output_dir, exist_ok=True)
+
+    isi_delta_signal_0_points = [(i - 2, (spikes_of_data_signals[0][i] - spikes_of_data_signals[0][i - 1]) -
+                                         (spikes_of_data_signals[0][i - 1] - spikes_of_data_signals[0][i - 2]))
+                                 for i in range(2, len(spikes_of_data_signals[0]))]
+
+    isi_delta_plain_0_points = [(i - 2, (trains[0].get_spikes()[i] - trains[0].get_spikes()[i - 1]) -
+                                        (trains[0].get_spikes()[i - 1] - trains[0].get_spikes()[i - 2]))
+                                for i in range(2, len(trains[0].get_spikes()))]
+
+    pathname = os.path.join(output_dir, "isi_delta_signal_0_curve", "isi_delta_signal_0.png")
+    print("    Saving plot " + pathname)
+    plot.curve_per_partes(
+        isi_delta_signal_0_points,
+        pathname,
+        0,
+        len(spikes_of_data_signals[0][2:]),
+        1000,
+        lambda p: print("    Saving plot " + p),
+        )
+
+    pathname = os.path.join(output_dir, "isi_delta_signal_0_hist.png")
+    print("    Saving plot " + pathname)
+    plot.histogram(
+        distribution.mkhist([p[1] for p in isi_delta_signal_0_points], 200)[0],
+        pathname,
+        normalised=False
+        )
+
+    pathname = os.path.join(output_dir, "isi_delta_plain_0_curve", "isi_delta_plain_0.png")
+    print("    Saving plot " + pathname)
+    plot.curve_per_partes(
+        isi_delta_plain_0_points,
+        pathname,
+        0,
+        len(trains[0].get_spikes()[2:]),
+        1000,
+        lambda p: print("    Saving plot " + p),
+        )
+
+    pathname = os.path.join(output_dir, "isi_delta_plain_0_hist.png")
+    print("    Saving plot " + pathname)
+    plot.histogram(
+        distribution.mkhist([p[1] for p in isi_delta_plain_0_points], 200)[0],
+        pathname,
+        normalised=False
+        )
+
+    pathname = os.path.join(output_dir, "hist_event_dist_0.png")
+    print("    Saving plot " + pathname)
+    plot.histogram(
+        data_signals[0].get_spiking_distribution(),
+        pathname,
+        normalised=True
+        )
+
+    pathname = os.path.join(output_dir, "isi_hist_plain_0.png")
+    print("    Saving plot " + pathname)
+    plot.histogram(
+        distribution.make_isi_histogram(
+            trains[0].get_spikes(),
+            nsteps,
+            start_time,
+            start_time + nsteps * dt
+            ),
+        pathname,
+        normalised=False
+        )
+
+    pathname = os.path.join(output_dir, "isi_hist_signal_0.png")
+    print("    Saving plot " + pathname)
+    plot.histogram(
+        distribution.make_isi_histogram(
+            spikes_of_data_signals[0],
+            nsteps,
+            start_time,
+            start_time + nsteps * dt
+            ),
+        pathname,
+        normalised=False
+        )
+
+    pathname = os.path.join(output_dir, "spikes_board", "spikes.png")
+    print("    Saving plot " + pathname)
+    plot.scatter_per_partes(
+        [(event, i) for i in range(num_spikers_per_kind) for event in trains[i].get_spikes()] +
+        [(event, num_spikers_per_kind + i) for i in range(num_spikers_per_kind) for event in spikes_of_data_signals[i]],
+        pathname,
+        start_time,
+        start_time + nsteps * dt,
+        plot_dt,
+        plot_stride,
+        lambda p: print("    Saving plot " + p),
+        ["C0" for i in range(num_spikers_per_kind) for _ in trains[i].get_spikes()] +
+        ["C2" for i in range(num_spikers_per_kind) for _ in spikes_of_data_signals[i]]
         )
 
     print("Done.")
