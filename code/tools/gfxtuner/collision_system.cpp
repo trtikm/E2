34abb4a0642f6e881998875e6c2f6bdc5eba0627
Marek Trtik <trtikm@gmail.com>
2018-08-25 15:15:59 +0100

    gfxtuner: Initial commit for collision system.

---
+++ code/tools/gfxtuner/collision_system.cpp
@@ -0,0 +1,5 @@
+#include <gfxtuner/collision_system.hpp>
+#include <utility/assumptions.hpp>
+#include <utility/invariants.hpp>
+#include <utility/timeprof.hpp>
+
---
+++ code/tools/gfxtuner/collision_system.hpp
@@ -0,0 +1,12 @@
+#ifndef E2_TOOL_GFXTUNER_COLLISION_SYSTEM_HPP_INCLUDED
+#   define E2_TOOL_GFXTUNER_COLLISION_SYSTEM_HPP_INCLUDED
+
+//#   include <gfxtuner/scene.hpp>
+
+
+struct collision_system
+{
+};
+
+
+#endif
