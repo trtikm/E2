1a63fcd0b826c42b40b00a8aa081a29ed6ab8bb1
Marek Trtik <trtikm@gmail.com>
2018-09-08 14:50:27 +0100

    Moving collision system from gfxtunner to angeo.

--- code/angeo/src/collision_system.cpp
+++ code/angeo/src/collision_system.cpp
@@ -1,4 +1,4 @@
-#include <gfxtuner/collision_system.hpp>
+#include <angeo/collision_system.hpp>
 #include <utility/assumptions.hpp>
 #include <utility/invariants.hpp>
 #include <utility/timeprof.hpp>
