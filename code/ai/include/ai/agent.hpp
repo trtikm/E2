8c0c331d68fe33b5c5c580ec386f360a4dc93ae2
Marek Trtik <trtikm@gmail.com>
2019-01-12 01:20:58 +0000

    ai: Added module ai (empty)

---
+++ code/ai/include/ai/agent.hpp
@@ -0,0 +1,16 @@
+#ifndef AI_AGENT_HPP_INCLUDED
+#   define AI_AGENT_HPP_INCLUDED
+
+#   include <utility/basic_numeric_types.hpp>
+
+namespace ai {
+
+
+struct agent
+{
+};
+
+
+}
+
+#endif
---
+++ code/ai/src/agent.cpp
@@ -0,0 +1,13 @@
+#include <ai/agent.hpp>
+#include <utility/assumptions.hpp>
+#include <utility/invariants.hpp>
+#include <utility/development.hpp>
+#include <utility/timeprof.hpp>
+#include <utility/log.hpp>
+
+namespace ai {
+
+
+
+
+}
