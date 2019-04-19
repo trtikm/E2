2b27695fd6ac4876b70a4d3d2bbcf167476c12e5
Marek Trtik <trtikm@gmail.com>
2019-04-19 22:56:58 +0100

    qtgl: Improved performance: use Qt method to convert QString to std::string.

--- code/qtgl/src/gui_utils.cpp
+++ code/qtgl/src/gui_utils.cpp
@@ -1,6 +1,7 @@
 #include <qtgl/gui_utils.hpp>
 #include <utility/assumptions.hpp>
 #include <utility/invariants.hpp>
+#include <utility/timeprof.hpp>
 
 namespace qtgl {
 
@@ -30,10 +31,7 @@ float_32_bit  get_splitter_sizes_ratio(QSplitter const&  splitter)
 
 std::string  to_string(QString const& s)
 {
-    std::string  result;
-    for (int  i = 0; i < s.size(); ++i)
-        result.push_back(s.at(i).toLatin1());
-    return result;
+    return s.toStdString();
 }
 
 
