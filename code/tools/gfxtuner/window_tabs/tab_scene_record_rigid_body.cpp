1f263fb8bce5ea806865e11532f2ed2693ca65d1
Marek Trtik <trtikm@gmail.com>
2019-05-06 14:04:24 +0100

    gfxtuner: rigid_body_dialog: inserting inverted forms of mass and inertia tensor directly.

--- code/tools/gfxtuner/window_tabs/tab_scene_record_rigid_body.cpp
+++ code/tools/gfxtuner/window_tabs/tab_scene_record_rigid_body.cpp
@@ -91,9 +91,9 @@ private:
 
     QCheckBox*  m_widget_auto_compute_mass_and_inertia_tensor;
 
-    QLineEdit*  m_widget_mass;
+    QLineEdit*  m_widget_inverted_mass;
 
-    std::array<std::array<QLineEdit*, 3>, 3>  m_widget_inertia_tensor;
+    std::array<std::array<QLineEdit*, 3>, 3>  m_widget_inverted_inertia_tensor;
 };
 
 
@@ -128,7 +128,7 @@ rigid_body_props_dialog::rigid_body_props_dialog(
         [](rigid_body_props_dialog* dlg, bool const  auto_compute_mass_and_inertia_tensor) {
             struct s : public QCheckBox {
                 s(rigid_body_props_dialog* dlg, bool const  auto_compute_mass_and_inertia_tensor)
-                    : QCheckBox("Compute mass and inertia tensor from colliders.")
+                    : QCheckBox("Compute inverted mass and inverted inertia tensor from colliders.")
                 {
                     setChecked(auto_compute_mass_and_inertia_tensor);
                     QObject::connect(this, &QCheckBox::stateChanged, dlg, &rigid_body_props_dialog::on_auto_compute_mass_and_inertia_tensor_changed);
@@ -139,16 +139,16 @@ rigid_body_props_dialog::rigid_body_props_dialog(
         
         )
 
-    , m_widget_mass(new QLineEdit)
+    , m_widget_inverted_mass(new QLineEdit)
 
-    , m_widget_inertia_tensor()
+    , m_widget_inverted_inertia_tensor()
 {
     ASSUMPTION(m_auto_compute_mass_and_inertia_tensor != nullptr);
     ASSUMPTION(m_props != nullptr);
 
     for (int i = 0; i != 3; ++i)
         for (int j = 0; j != 3; ++j)
-            m_widget_inertia_tensor[i][j] = new QLineEdit;
+            m_widget_inverted_inertia_tensor[i][j] = new QLineEdit;
 
     m_widget_linear_velocity_x->setText(QString::number(m_props->m_linear_velocity(0)));
     m_widget_linear_velocity_y->setText(QString::number(m_props->m_linear_velocity(1)));
@@ -168,23 +168,12 @@ rigid_body_props_dialog::rigid_body_props_dialog(
 
     m_widget_auto_compute_mass_and_inertia_tensor->setChecked(*m_auto_compute_mass_and_inertia_tensor);
 
-    if (m_props->m_mass_inverted < 0.0001f)
-        m_widget_mass->setText("0.0");
-    else
-        m_widget_mass->setText(QString::number(1.0f / m_props->m_mass_inverted));
+    m_widget_inverted_mass->setText(QString::number(m_props->m_mass_inverted));
 
-    if (is_zero(m_props->m_inertia_tensor_inverted))
-        for (int i = 0; i != 3; ++i)
-            for (int j = 0; j != 3; ++j)
-                m_widget_inertia_tensor[i][j]->setText("0.0");
-    else
-    {
-        matrix33  M = inverse33(m_props->m_inertia_tensor_inverted);
-        for (int i = 0; i != 3; ++i)
-            for (int j = 0; j != 3; ++j)
-                m_widget_inertia_tensor[i][j]->setText(QString::number(M(i,j)));
-    }
-    
+    for (int i = 0; i != 3; ++i)
+        for (int j = 0; j != 3; ++j)
+            m_widget_inverted_inertia_tensor[i][j]->setText(QString::number(m_props->m_inertia_tensor_inverted(i, j)));
+
     m_widget_linear_velocity_x->setToolTip("x coordinate in world space.");
     m_widget_linear_velocity_y->setToolTip("y coordinate in world space.");
     m_widget_linear_velocity_z->setToolTip("z coordinate in world space.");
@@ -202,18 +191,17 @@ rigid_body_props_dialog::rigid_body_props_dialog(
     m_widget_external_angular_acceleration_z->setToolTip("z coordinate in world space.");
 
     m_widget_auto_compute_mass_and_inertia_tensor->setToolTip(
-            "Whether to automatically compute mass and innertia\n"
-            "tensor from colliders under the scene node with this\n"
-            "rigid body, or to manually specify the mass and the\n"
-            "inertia tensor using the widgets below."
+            "Whether to automatically compute inverted mass and inverted innertia tensor\n"
+            "from colliders under the scene node with this rigid body, or to manually\n"
+            "specify the mass and the inertia tensor using the widgets below."
             );
 
-    m_widget_mass->setToolTip("Mass of the rigid body.");
+    m_widget_inverted_mass->setToolTip("Inverted mass of the rigid body.");
     for (int i = 0; i != 3; ++i)
         for (int j = 0; j != 3; ++j)
         {
-            std::string const  msg = msgstream() << "Element (" << i << "," << j << ") of the inertia tensor.";
-            m_widget_inertia_tensor[i][j]->setToolTip(QString(msg.c_str()));
+            std::string const  msg = msgstream() << "Element (" << i << "," << j << ") of the inverted inertia tensor.";
+            m_widget_inverted_inertia_tensor[i][j]->setToolTip(QString(msg.c_str()));
         }
 
     set_enable_state_of_mass_and_inertia_tensor(!*m_auto_compute_mass_and_inertia_tensor);
@@ -267,37 +255,47 @@ rigid_body_props_dialog::rigid_body_props_dialog(
 
         dlg_layout->addWidget(m_widget_auto_compute_mass_and_inertia_tensor);
 
-        QWidget* const mass_and_inertia_tensor_group = new QGroupBox("Mass and inertia tensor");
+        QWidget* const mass_and_inertia_tensor_group = new QGroupBox("Inverted mass and inverted inertia tensor");
         {
+            mass_and_inertia_tensor_group->setToolTip(
+                    "In these widgets you can manually specify distribution of mass in the local\n"
+                    "coordinate system (i.e. frame) of the rigid body.\n"
+                    "NOTE: Insertion of mass and inertia tensor in the inverted forms has advantage\n"
+                    "that you can also define unreal rigid bodies, i.e. those for which the inverted\n"
+                    "forms normally do no exist. For example, rigid body with zero inverted mass\n"
+                    "and zero inverted inertia tensor cannot be moved by any force or torque;\n"
+                    "when all elements of the inverted inertia tensor are zero except (2,2),\n"
+                    "then the rigid body can rotate only along z-axis (of its frame)."
+                    );
             QVBoxLayout* const group_layout = new QVBoxLayout;
             {
                 QHBoxLayout* const mass_layout = new QHBoxLayout;
                 {
-                    mass_layout->addWidget(new QLabel("Mass [kg]: "));
-                    mass_layout->addWidget(m_widget_mass);
+                    mass_layout->addWidget(new QLabel("Inverted mass [1/kg]: "));
+                    mass_layout->addWidget(m_widget_inverted_mass);
                 }
                 group_layout->addLayout(mass_layout);
 
-                group_layout->addWidget(new QLabel("Inertia tensor:"));
+                group_layout->addWidget(new QLabel("Inverted inertia tensor (in the local frame of the ridid body):"));
                 QHBoxLayout* const inertia_tensor_row_1_layout = new QHBoxLayout;
                 {
-                    inertia_tensor_row_1_layout->addWidget(m_widget_inertia_tensor[0][0]);
-                    inertia_tensor_row_1_layout->addWidget(m_widget_inertia_tensor[0][1]);
-                    inertia_tensor_row_1_layout->addWidget(m_widget_inertia_tensor[0][2]);
+                    inertia_tensor_row_1_layout->addWidget(m_widget_inverted_inertia_tensor[0][0]);
+                    inertia_tensor_row_1_layout->addWidget(m_widget_inverted_inertia_tensor[0][1]);
+                    inertia_tensor_row_1_layout->addWidget(m_widget_inverted_inertia_tensor[0][2]);
                 }
                 group_layout->addLayout(inertia_tensor_row_1_layout);
                 QHBoxLayout* const inertia_tensor_row_2_layout = new QHBoxLayout;
                 {
-                    inertia_tensor_row_2_layout->addWidget(m_widget_inertia_tensor[1][0]);
-                    inertia_tensor_row_2_layout->addWidget(m_widget_inertia_tensor[1][1]);
-                    inertia_tensor_row_2_layout->addWidget(m_widget_inertia_tensor[1][2]);
+                    inertia_tensor_row_2_layout->addWidget(m_widget_inverted_inertia_tensor[1][0]);
+                    inertia_tensor_row_2_layout->addWidget(m_widget_inverted_inertia_tensor[1][1]);
+                    inertia_tensor_row_2_layout->addWidget(m_widget_inverted_inertia_tensor[1][2]);
                 }
                 group_layout->addLayout(inertia_tensor_row_2_layout);
                 QHBoxLayout* const inertia_tensor_row_3_layout = new QHBoxLayout;
                 {
-                    inertia_tensor_row_3_layout->addWidget(m_widget_inertia_tensor[2][0]);
-                    inertia_tensor_row_3_layout->addWidget(m_widget_inertia_tensor[2][1]);
-                    inertia_tensor_row_3_layout->addWidget(m_widget_inertia_tensor[2][2]);
+                    inertia_tensor_row_3_layout->addWidget(m_widget_inverted_inertia_tensor[2][0]);
+                    inertia_tensor_row_3_layout->addWidget(m_widget_inverted_inertia_tensor[2][1]);
+                    inertia_tensor_row_3_layout->addWidget(m_widget_inverted_inertia_tensor[2][2]);
                 }
                 group_layout->addLayout(inertia_tensor_row_3_layout);
             }
@@ -349,10 +347,10 @@ void rigid_body_props_dialog::on_auto_compute_mass_and_inertia_tensor_changed(in
 
 void  rigid_body_props_dialog::set_enable_state_of_mass_and_inertia_tensor(bool const  set_enabled)
 {
-    m_widget_mass->setEnabled(set_enabled);
+    m_widget_inverted_mass->setEnabled(set_enabled);
     for (int i = 0; i != 3; ++i)
         for (int j = 0; j != 3; ++j)
-            m_widget_inertia_tensor[i][j]->setEnabled(set_enabled);
+            m_widget_inverted_inertia_tensor[i][j]->setEnabled(set_enabled);
 }
 
 
@@ -386,21 +384,12 @@ void  rigid_body_props_dialog::accept()
 
     if (*m_auto_compute_mass_and_inertia_tensor == false)
     {
-        float_32_bit const  mass = (float_32_bit)std::atof(qtgl::to_string(m_widget_mass->text()).c_str());
-        if (mass < 0.0001f)
-            m_props->m_mass_inverted = 0.0f;
-        else
-            m_props->m_mass_inverted = 1.0f / (float_32_bit)std::atof(qtgl::to_string(m_widget_mass->text()).c_str());
+        m_props->m_mass_inverted = (float_32_bit)std::atof(qtgl::to_string(m_widget_inverted_mass->text()).c_str());
 
-        matrix33  M;
         for (int i = 0; i != 3; ++i)
             for (int j = 0; j != 3; ++j)
-                M(i,j) = (float_32_bit)std::atof(qtgl::to_string(m_widget_inertia_tensor[i][j]->text()).c_str());
-        matrix33  Minv = inverse33(M);
-        if (!is_valid(Minv) || is_zero(M))
-            m_props->m_inertia_tensor_inverted = matrix33_zero();
-        else
-            m_props->m_inertia_tensor_inverted = Minv;
+                m_props->m_inertia_tensor_inverted(i,j) =
+                        (float_32_bit)std::atof(qtgl::to_string(m_widget_inverted_inertia_tensor[i][j]->text()).c_str());
     }        
 
     m_ok = true;
