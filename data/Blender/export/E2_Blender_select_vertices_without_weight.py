359671caa03a736bdeff0e005bac7fe0e410e972
Marek Trtik <trtikm@gmail.com>
2019-07-07 14:47:01 +0100

    blender: improved script for selection of wrongly weighted vertices.

--- data/Blender/export/E2_Blender_select_vertices_without_weight.py
+++ data/Blender/export/E2_Blender_select_vertices_without_weight.py
@@ -6,6 +6,13 @@ will refuse to export object with a vertex not attached to any bone).
 
 import bpy
 
+# --- CONFIGURATION SECTION -------------------------------
+
+SELECT_WITHOUT_WEIGHT = True
+SELECT_NOT_NORMALISED = False
+
+# --- THE IMPLEMENTATION SECTION (do not modify) ------------------------
+
 print("*************************************************************************************")
 bpy.ops.object.mode_set(mode='OBJECT')
 vertices = bpy.context.active_object.data.vertices
@@ -14,8 +21,18 @@ bpy.ops.mesh.select_mode(type="VERT")
 bpy.ops.mesh.select_all(action='DESELECT')
 bpy.ops.object.mode_set(mode='OBJECT')
 
-for idx in range(len(vertices)):
-    if len(vertices[idx].groups) == 0:
-        vertices[idx].select = True
+
+if SELECT_WITHOUT_WEIGHT is True:
+    for idx in range(len(vertices)):
+        if len(vertices[idx].groups) == 0:
+            vertices[idx].select = True
+
+if SELECT_NOT_NORMALISED is True:
+    for idx in range(len(vertices)):
+        s = 0.0
+        for g in vertices[idx].groups:
+            s += g.weight
+        if s < 0.999:
+            vertices[idx].select = True
 
 bpy.ops.object.mode_set(mode='EDIT')
