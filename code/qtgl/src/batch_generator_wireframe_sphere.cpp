99a3f3c2271cb83dbe832ad8cd0d54ddf4b2c778
Marek Trtik <trtikm@gmail.com>
2020-03-07 13:53:02 +0000

    atgl: added implementation of create_solid_sphere

--- code/qtgl/src/batch_generator_wireframe_sphere.cpp
+++ code/qtgl/src/batch_generator_wireframe_sphere.cpp
@@ -133,7 +133,69 @@ batch  create_solid_sphere(
     ASSUMPTION(radius > 1e-4f);
     ASSUMPTION(num_lines_per_quarter_of_circle != 0U);
 
-    NOT_IMPLEMENTED_YET();
+    natural_16_bit const  num_vertices_in_horisontal_slice = num_lines_per_quarter_of_circle * 4U;
+    natural_16_bit const  num_horisontal_slices = 2U * (num_lines_per_quarter_of_circle - 1U) + 1U;
+
+    natural_16_bit const  num_vertices = num_horisontal_slices * num_vertices_in_horisontal_slice + 2U;
+
+    natural_16_bit const  num_indices = 2U * (num_horisontal_slices - 1U) * num_vertices_in_horisontal_slice +
+                                        2U * num_vertices_in_horisontal_slice;
+
+    std::vector< std::array<float_32_bit, 3> >  vertices;
+    std::vector< std::array<float_32_bit, 3> >  normals;
+    {
+        vertices.reserve(num_vertices);
+        normals.reserve(num_vertices);
+        float_32_bit const  delta_phi = (PI() / 2.0f) / static_cast<float_32_bit>(num_lines_per_quarter_of_circle);
+        for (integer_32_bit  i = -(integer_32_bit)(num_lines_per_quarter_of_circle - 1U); i < num_lines_per_quarter_of_circle; ++i)
+        {
+            float_32_bit const  psi = static_cast<float_32_bit>(i)* delta_phi;
+            float_32_bit const  cos_psi = std::cosf(psi);
+            float_32_bit const  sin_psi = std::sinf(psi);
+            for (integer_32_bit  j = 0U; j < num_vertices_in_horisontal_slice; ++j)
+            {
+                float_32_bit const  phi = static_cast<float_32_bit>(j)* delta_phi;
+                float_32_bit const  cos_phi = std::cosf(phi);
+                float_32_bit const  sin_phi = std::sinf(phi);
+                vector3 const  v( cos_phi * cos_psi, sin_phi * cos_psi, sin_psi);                
+                vertices.push_back({ radius * v(0), radius * v(1), radius * v(2) });
+                normals.push_back({ v(0), v(1), v(2) });
+            }
+        }
+        vertices.push_back({ 0.0f, 0.0f, -radius });
+        normals.push_back({ 0.0f, 0.0f, -1.0f });
+        vertices.push_back({ 0.0f, 0.0f, radius });
+        normals.push_back({ 0.0f, 0.0f, 1.0f });
+        INVARIANT(vertices.size() == num_vertices && normals.size() == num_vertices);
+    }
+
+    std::vector< std::array<natural_32_bit, 3> >  indices;
+    {
+        indices.reserve(num_indices);
+        for (natural_16_bit i = num_vertices_in_horisontal_slice - 1U, j = 0U; j < num_vertices_in_horisontal_slice; i = j, ++j)
+            indices.push_back({ j, i, num_vertices - 2U });
+        for (natural_16_bit k = 0U; k + 1U < num_horisontal_slices; ++k)
+            for (natural_16_bit  i = num_vertices_in_horisontal_slice - 1U, j = 0U; j < num_vertices_in_horisontal_slice; i = j, ++j)
+            {
+                natural_32_bit const  lo_shift = k * num_vertices_in_horisontal_slice;
+                natural_32_bit const  hi_shift = lo_shift + num_vertices_in_horisontal_slice;
+                indices.push_back({ lo_shift + i, lo_shift + j, hi_shift + j });
+                indices.push_back({ lo_shift + i, hi_shift + j, hi_shift + i });
+            }
+        natural_32_bit const  shift = num_vertices - 2U - num_vertices_in_horisontal_slice;
+        for (natural_16_bit i = num_vertices_in_horisontal_slice - 1U, j = 0U; j < num_vertices_in_horisontal_slice; i = j, ++j)
+            indices.push_back({ shift + i, shift + j, num_vertices - 1U });
+        INVARIANT(indices.size() == num_indices);
+    }
+
+    return create_triangle_mesh(
+                vertices,
+                indices,
+                normals,
+                colour,
+                fog_type_,
+                id.empty() ? make_sphere_id_without_prefix(radius, num_lines_per_quarter_of_circle, colour, fog_type_, false) : id
+                );
 }
 
 
