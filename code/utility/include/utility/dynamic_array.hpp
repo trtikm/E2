fe002c671c5a3dd86e117e990d475be01a51e286
Marek Trtik <trtikm@gmail.com>
2020-08-09 02:30:20 +0100

    utility: added const iterator to dynamic_array

--- code/utility/include/utility/dynamic_array.hpp
+++ code/utility/include/utility/dynamic_array.hpp
@@ -14,6 +14,33 @@ struct  dynamic_array
     using  data_vector = std::vector<element_type>;
     using  indices_set = std::unordered_set<element_index>;
 
+    struct  const_iterator
+    {
+        using value_type      = element_type;
+        using difference_type = element_index;
+        using pointer         = value_type const*;
+        using reference       = value_type const&;
+
+        using container_type        = indices_set;
+        using indices_iterator_type = typename container_type::const_iterator;
+
+        const_iterator() noexcept : indices_it(), data(nullptr) {}
+        const_iterator(indices_iterator_type const it, data_vector const* const  data_) noexcept : indices_it(it), data(data_) {}
+
+        reference  operator*() const { return data->at(*indices_it); }
+
+        const_iterator&  operator++() { ++indices_it; return *this; }
+        const_iterator  operator++(int) { const_iterator  tmp = *this; ++indices_it; return tmp; }
+
+        bool  operator==(const_iterator const&  other) const { return indices_it == other.indices_it && data == other.data; }
+        bool  operator!=(const_iterator const&  other) const { return !(*this == other); }
+
+    private:
+
+        indices_iterator_type  indices_it;
+        data_vector const*  data;
+    };
+
     element_index  insert(element_type const&  value = element_type());
     void  erase(element_index const  idx) { at(idx) = element_type(); m_valid_indices.erase(idx); m_free_indices.insert(idx); }
     void  clear() { m_data.clear(); m_valid_indices.clear(); m_free_indices.clear(); }
@@ -24,6 +51,9 @@ struct  dynamic_array
 
     element_type const&  at(element_index const  idx) const { return m_data.at(idx); }
     element_type&  at(element_index const  idx) { return m_data.at(idx); }
+
+    const_iterator  begin() const { return const_iterator(m_valid_indices.begin(), &m_data); }
+    const_iterator  end() const { return const_iterator(m_valid_indices.end(), &m_data); }
 
     data_vector const&  data() const { return m_data; }
     indices_set const&  valid_indices() const { return m_valid_indices; }
