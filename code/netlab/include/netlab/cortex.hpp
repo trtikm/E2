3fbe2f6fa1d4eeba58cf2f2996e742fa6d7e6dd2
Marek Trtik <trtikm@gmail.com>
2020-08-23 01:52:16 +0100

    netlab: use leaky IF neuron; simpler weight update for inhib. synapses; improved API.

--- code/netlab/include/netlab/cortex.hpp
+++ code/netlab/include/netlab/cortex.hpp
@@ -22,50 +22,68 @@ struct  cortex
         neuron_index  neuron;
     };
 
-    struct  neuron // Izhikevich model
+    struct  neuron
     {
-        struct integration_constants
+        struct constant_data
         {
-            float_32_bit  treashold; // in mV
-            float_32_bit  a;
-            float_32_bit  b;
-            float_32_bit  c;
-            float_32_bit  d;
+            float_32_bit  excitation_initial; // in <-1,1>; default 0.0f
+            float_32_bit  spiking_excitation_initial; // in <-1,1>; default 0.9f
+            float_32_bit  excitation_recovery; // in <-1,1>; default 0.0f
+            float_32_bit  ln_of_excitation_decay_coef; // <=0; default -3.0f
+            float_32_bit  max_input_signal_magnitude; // >0; default is the number of dendrites.
         };
 
-        float_32_bit  membrane_voltage; // in mV
-        float_32_bit  membrane_voltage_derivative;
-        float_32_bit  recovery_voltage; // in mV
-        float_32_bit  recovery_voltage_derivative;
-        float_32_bit  input_current; // in mA
+        float_32_bit  excitation; // in <-1, 1>
+        float_32_bit  spiking_excitation; // in <0, 1>
+        float_32_bit  input_signal; // in <-D,D>, where D is number of dendrites of the neuron
     };
 
     struct  synapse // Hebbian pasticity model; weight treashold used for disconnection
     {
+        struct constant_data
+        {
+            float_32_bit  weight_initial; // in <0,1>; default 0.5
+            float_32_bit  weight_disconnection; // in <0,1>; default 0.1
+            float_32_bit  weight_delta_per_second; // in <0,1>; default 0.25
+            float_32_bit  weight_decay_delta_per_second; // in <0,1>; default 0.01
+        };
+
         neuron_guid  from;
         neuron_guid  to;
-        float_32_bit  weight;
+        float_32_bit  weight; // in <0,1>
     };
 
     struct  layer
     {
-        // --- CONSTANTS ------------------------------------------------------------------------
-        neuron::integration_constants  integration_constants; // From the Izhikevich model.
-        float_32_bit  sign; // Sign of an action potential: 1.0f - excitatory, -1.0f - inhibitory.
-        std::vector<float_32_bit>  conductances; // For conversion of voltage derivative to current - for each layer one coef > 0.
-        std::vector<float_32_bit>  weight_resistances; // Scale For update of synaptic weight from change in pre- and post- synaptic
-                                                       // voltage changes - for each layer one resistance > 0.
-        std::vector<float_32_bit>  disconnection_weights; // For diconnection of synapses - For each layer one number > 0.
-        std::vector<float_32_bit>  initial_weights; // For newly created synapse - for each layer one weight > corresponding
-                                                    // disconnection_weight.
-        std::vector<float_32_bit>  connection_probabilities; // For connection of synapses - For each layer one number >= 0.
-        std::vector<float_32_bit>  accumulated_connection_probabilities; // i-th number is the normalised sum of all probabilities
-                                                                         // connection_probabilities[0..i]. The last number is 1.0f.
+        struct  constant_data
+        {
+            neuron::constant_data  neuron;
 
-        // --- DATA ------------------------------------------------------------------------
+            float_32_bit  sign; // Sign of the output action potential: 1.0f - excitatory, -1.0f - inhibitory.
+            natural_16_bit  num_axons_per_neuron;
+            natural_16_bit  num_dendrites_per_neuron;
+
+            struct  to_layer_data
+            {
+                float_32_bit  connection_probability; // in <0,1>
+                float_32_bit  accumulated_connection_probability; // >= 0
+
+                synapse::constant_data  synapse;
+
+            };
+            std::vector<to_layer_data>  to_layer;
+
+        } constants;
+
         std::vector<neuron>  neurons; // Fixed size.
         std::vector<neuron_index>  free_axons;
         std::vector<neuron_index>  free_dendrites;
+    };
+
+    struct  constant_data
+    {
+        //float_32_bit  simulation_fequency;
+        //float_32_bit  expected_spiking_frequency;
     };
 
     ////////////////////////////////////////////////////////////////////////////////////////
@@ -76,6 +94,8 @@ struct  cortex
 
     void  reset_random_generator(natural_32_bit const  seed) { random_generator.seed(seed); }
 
+    void  clear();
+
     layer_index  add_layer(
                 natural_16_bit const  num_neurons,
                 natural_16_bit const  num_axons_per_neuron,
@@ -83,24 +103,21 @@ struct  cortex
                 bool const  excitatory
                 );
 
-    void  set_integration_constants(layer_index const  index, neuron::integration_constants const&  constants)
-    { layers.at(index).integration_constants = constants;}
+    //void  set_constant_expected_spiking_frequency(float_32_bit const  value);
 
-    void  set_conductance(layer_index const  from, layer_index const  to, float_32_bit const  value)
-    { layers.at(from).conductances.at(to) = value; }
+    void  set_constant_connection_probability(layer_index const  from, layer_index const  to, float_32_bit const  value);
 
-    void  set_weight_resistance(layer_index const  from, layer_index const  to, float_32_bit const  value)
-    { layers.at(from).weight_resistances.at(to) = value; }
+    void  set_constant_neuron_excitation_initial(layer_index const  index, float_32_bit const  value);
+    void  set_constant_neuron_spiking_excitation_initial(layer_index const  index, float_32_bit const  value);
+    void  set_constant_neuron_excitation_recovery(layer_index const  index, float_32_bit const  value);
+    void  set_constant_neuron_ln_of_excitation_decay_coef(layer_index const  index, float_32_bit const  value);
+    void  set_constant_neuron_max_input_signal_magnitude(layer_index const  index, float_32_bit const  value);
 
-    void  set_disconnection_weight(layer_index const  from, layer_index const  to, float_32_bit const  value)
-    { layers.at(from).disconnection_weights.at(to) = value; }
-
-    void  set_initial_weight(layer_index const  from, layer_index const  to, float_32_bit const  value)
-    { layers.at(from).initial_weights.at(to) = value; }
-
-    void  set_connection_probability(layer_index const  from, layer_index const  to, float_32_bit const  value)
-    { layer& l = layers.at(from); l.connection_probabilities.at(to) = value; recompute_accumulated_connection_probabilities(l); }
-
+    void  set_constant_synapse_weight_initial(layer_index const  from, layer_index const  to, float_32_bit const  value);
+    void  set_constant_synapse_weight_disconnection(layer_index const  from, layer_index const  to, float_32_bit const  value);
+    void  set_constant_synapse_weight_delta_per_second(layer_index const  from, layer_index const  to, float_32_bit const  value);
+    void  set_constant_synapse_weight_decay_delta_per_second(layer_index const  from, layer_index const  to, float_32_bit const  value);
+    
     void  build_new_synapses();
 
     ////////////////////////////////////////////////////////////////////////////////////////
@@ -111,17 +128,25 @@ struct  cortex
     natural_32_bit  num_synapses() const { return (natural_32_bit)synapses.size(); }
 
     layer const&  get_layer(layer_index const  index) const { return layers.at(index); }
+    layer&  layer_ref(layer_index const  index) { return layers.at(index); }
+
     synapse const&  get_synapse(synapse_index const  index) const { return synapses.at(index); }
+    synapse&  synapse_ref(synapse_index const  index) { return synapses.at(index); }
+
+    neuron const&  get_neuron(neuron_guid const  guid) const { return layers.at(guid.layer).neurons.at(guid.neuron); }
+    neuron&  neuron_ref(neuron_guid const  guid) { return layers.at(guid.layer).neurons.at(guid.neuron); }
+
+    constant_data const&  get_constants() const { return constants; }
+    layer::constant_data const&  get_layer_constants(layer_index const  index) const { return layers.at(index).constants; }
 
     ////////////////////////////////////////////////////////////////////////////////////////
     // I/O
     ////////////////////////////////////////////////////////////////////////////////////////
 
-    void  add_input_current_in_mA(neuron_guid const  guid, float_32_bit const  input_current_delta)
-    { layers.at(guid.layer).neurons.at(guid.neuron).input_current += input_current_delta; }
+    void  add_to_input_signal(neuron_guid const  guid, float_32_bit const  input_signal_delta)
+    { layers.at(guid.layer).neurons.at(guid.neuron).input_signal += input_signal_delta; }
 
-    float_32_bit  membrane_voltage_in_mV(neuron_guid const  guid) const
-    { return layers.at(guid.layer).neurons.at(guid.neuron).membrane_voltage; }
+    float_32_bit  neuron_excitation(neuron_guid const  guid) const { return get_neuron(guid).excitation; }
 
     ////////////////////////////////////////////////////////////////////////////////////////
     // SIMULATION
@@ -129,12 +154,15 @@ struct  cortex
 
     void  next_round(float_32_bit const  round_seconds);
 
-private:
     void  update_neurons(float_32_bit const  round_seconds);
+    void  clear_input_signal_of_neurons();
     void  update_existing_synapses(float_32_bit const  round_seconds);
+    void  disconnect_weak_synapses();
 
+private:
     void  recompute_accumulated_connection_probabilities(layer&  l);
 
+    constant_data  constants;
     std::vector<layer>  layers;
     std::vector<synapse>  synapses;
     random_generator_for_natural_32_bit  random_generator;
--- code/netlab/src/cortex.cpp
+++ code/netlab/src/cortex.cpp
@@ -1,4 +1,5 @@
 #include <netlab/cortex.hpp>
+#include <angeo/tensor_math.hpp>
 #include <utility/assumptions.hpp>
 #include <utility/invariants.hpp>
 #include <utility/timeprof.hpp>
@@ -8,7 +9,11 @@ namespace netlab {
 
 
 cortex::cortex()
-    : layers()
+    : constants{
+        //100.0f,     // simulation_fequency
+        //10.0f       // expected_spiking_frequency
+        }
+    , layers()
     , synapses()
     , random_generator(1U)
 {}
@@ -17,12 +22,20 @@ cortex::cortex()
 void  cortex::recompute_accumulated_connection_probabilities(layer&  l)
 {
     float_32_bit  sum = 0.0f;
-    for (layer_index  i = 0U, n =(synapse_index)layers.size(); i != n; ++i, sum = l.accumulated_connection_probabilities.at(i))
-        l.accumulated_connection_probabilities.at(i) = sum + l.connection_probabilities.at(i);
+    for (layer_index  i = 0U, n =(synapse_index)layers.size(); i != n;
+            sum = l.constants.to_layer.at(i).accumulated_connection_probability, ++i)
+        l.constants.to_layer.at(i).accumulated_connection_probability = sum + l.constants.to_layer.at(i).connection_probability;
     if (sum > 1e-6f)
-        for (float_32_bit&  ap : l.accumulated_connection_probabilities)
-            ap /= sum;
-    l.accumulated_connection_probabilities.back() = 1.0f;
+        for (layer::constant_data::to_layer_data&  data : l.constants.to_layer)
+            data.accumulated_connection_probability /= sum;
+    l.constants.to_layer.back().accumulated_connection_probability = 1.0f;
+}
+
+
+void  cortex::clear()
+{
+    layers.clear();
+    synapses.clear();
 }
 
 
@@ -35,57 +48,164 @@ cortex::layer_index  cortex::add_layer(
 {
     for (layer&  l : layers)
     {
-        l.conductances.push_back(1.0f);
-        l.weight_resistances.push_back(1.0f);
-        l.disconnection_weights.push_back(0.0f);
-        l.initial_weights.push_back(1.0f);
-        l.connection_probabilities.push_back(1.0f);
+        l.constants.to_layer.push_back({});
+        layer::constant_data::to_layer_data&  data = l.constants.to_layer.back();
+
+        data.connection_probability = 1.0f;
         recompute_accumulated_connection_probabilities(l);
+
+        data.synapse.weight_initial = 0.5f;
+        data.synapse.weight_disconnection = 0.1f;
+        data.synapse.weight_delta_per_second = 0.25f;
+        data.synapse.weight_decay_delta_per_second = 0.01f;
     }
 
     layers.push_back({});
 
     layer&  l = layers.back();
 
-    // Setup for Izhikevich regular spiking neuron.
-    l.integration_constants.treashold = 30.0f;
-    l.integration_constants.a = 0.02f;
-    l.integration_constants.b = 0.2f;
-    l.integration_constants.c = -65.0f;
-    l.integration_constants.d = 8.0f;
+    l.constants.sign = excitatory ? 1.0f : -1.0f;
+    l.constants.num_axons_per_neuron = num_axons_per_neuron;
+    l.constants.num_dendrites_per_neuron = num_dendrites_per_neuron;
+    l.constants.neuron.excitation_initial = 0.0f;
+    l.constants.neuron.spiking_excitation_initial = 0.9f;
+    l.constants.neuron.excitation_recovery = 0.0f;
+    l.constants.neuron.ln_of_excitation_decay_coef = -3.0f;
+    l.constants.neuron.max_input_signal_magnitude = (float_32_bit)l.constants.num_dendrites_per_neuron;
 
-    l.sign = excitatory ? 1.0f : -1.0f;
-
+    l.constants.to_layer.reserve(layers.size());
     for (layer_index  i = 0U, n =(synapse_index)layers.size(); i != n; ++i)
     {
-        l.conductances.push_back(1.0f);
-        l.weight_resistances.push_back(1.0f);
-        l.disconnection_weights.push_back(0.0f);
-        l.initial_weights.push_back(1.0f);
-        l.accumulated_connection_probabilities.push_back(1.0f);
+        l.constants.to_layer.push_back({});
+        layer::constant_data::to_layer_data&  data = l.constants.to_layer.back();
+
+        data.connection_probability = 1.0f;
+
+        data.synapse.weight_initial = 0.5f;
+        data.synapse.weight_disconnection = 0.1f;
+        data.synapse.weight_delta_per_second = 0.25f;
+        data.synapse.weight_decay_delta_per_second = 0.01f;
     }
+    recompute_accumulated_connection_probabilities(l);
 
     l.neurons.resize(num_neurons);
     for (neuron&  n : l.neurons)
     {
-        n.membrane_voltage = l.integration_constants.c;
-        n.membrane_voltage_derivative = 0.0f;
-        n.recovery_voltage = -0.5f * l.integration_constants.treashold;
-        n.recovery_voltage_derivative = 0.0f;
-        n.input_current = 0.0f;
+        n.excitation = l.constants.neuron.excitation_initial;
+        n.spiking_excitation = l.constants.neuron.spiking_excitation_initial;
+        n.input_signal = 0.0f;
     }
 
-    l.free_axons.reserve(num_neurons * num_axons_per_neuron);
+    l.free_axons.reserve(num_neurons * l.constants.num_axons_per_neuron);
     for (neuron_index  i = 0U, n =(neuron_index)l.neurons.size(); i != n; ++i)
-        for (axon_index  j = 0U; i != num_axons_per_neuron; ++j)
+        for (axon_index  j = 0U; j != l.constants.num_axons_per_neuron; ++j)
             l.free_axons.push_back(i);
 
-    l.free_dendrites.reserve(num_neurons * num_dendrites_per_neuron);
+    l.free_dendrites.reserve(num_neurons * l.constants.num_dendrites_per_neuron);
     for (neuron_index  i = 0U, n =(neuron_index)l.neurons.size(); i != n; ++i)
-        for (axon_index  j = 0U; i != num_dendrites_per_neuron; ++j)
+        for (axon_index  j = 0U; j != l.constants.num_dendrites_per_neuron; ++j)
             l.free_dendrites.push_back(i);
 
     return (layer_index)layers.size() - 1U;
+}
+
+
+//void  cortex::set_constant_expected_spiking_frequency(float_32_bit const  value)
+//{
+//    ASSUMPTION(value >= 1.0f);
+//    constants.expected_spiking_frequency = value;
+//}
+
+
+void  cortex::set_constant_connection_probability(
+        layer_index const  from, layer_index const  to, float_32_bit const  value
+        )
+{
+    ASSUMPTION(value >= 0.0f);
+    layer&  l = layers.at(from);
+    l.constants.to_layer.at(to).connection_probability = value;
+    recompute_accumulated_connection_probabilities(l);
+}
+
+
+void  cortex::set_constant_neuron_excitation_initial(
+        layer_index const  index, float_32_bit const  value
+        )
+{
+    ASSUMPTION(-1.0f <= value && value <= 1.0f);
+    layers.at(index).constants.neuron.excitation_initial = value;
+}
+
+
+void  cortex::set_constant_neuron_spiking_excitation_initial(
+        layer_index const  index, float_32_bit const  value
+        )
+{
+    ASSUMPTION(-1.0f <= value && value <= 1.0f);
+    layers.at(index).constants.neuron.spiking_excitation_initial = value;
+}
+
+
+void  cortex::set_constant_neuron_excitation_recovery(
+        layer_index const  index, float_32_bit const  value
+        )
+{
+    ASSUMPTION(-1.0f <= value && value <= 1.0f);
+    layers.at(index).constants.neuron.excitation_recovery = value;
+}
+
+
+void  cortex::set_constant_neuron_ln_of_excitation_decay_coef(
+        layer_index const  index, float_32_bit const  value
+        )
+{
+    ASSUMPTION(value <= 0.0f);
+    layers.at(index).constants.neuron.ln_of_excitation_decay_coef = value;
+}
+
+
+void  cortex::set_constant_neuron_max_input_signal_magnitude(
+        layer_index const  index, float_32_bit const  value
+        )
+{
+    ASSUMPTION(0.0001f < value);
+    layers.at(index).constants.neuron.max_input_signal_magnitude = value;
+}
+
+
+void  cortex::set_constant_synapse_weight_initial(
+        layer_index const  from, layer_index const  to, float_32_bit const  value
+        )
+{
+    ASSUMPTION(0.0f <= value && value <= 1.0f);
+    layers.at(from).constants.to_layer.at(to).synapse.weight_initial = value;
+}
+
+
+void  cortex::set_constant_synapse_weight_disconnection(
+        layer_index const  from, layer_index const  to, float_32_bit const  value
+        )
+{
+    ASSUMPTION(0.0f <= value && value <= 1.0f);
+    layers.at(from).constants.to_layer.at(to).synapse.weight_disconnection = value;
+}
+
+
+void  cortex::set_constant_synapse_weight_delta_per_second(
+        layer_index const  from, layer_index const  to, float_32_bit const  value
+        )
+{
+    ASSUMPTION(0.0f <= value && value <= 1.0f);
+    layers.at(from).constants.to_layer.at(to).synapse.weight_delta_per_second = value;
+}
+
+
+void  cortex::set_constant_synapse_weight_decay_delta_per_second(
+        layer_index const  from, layer_index const  to, float_32_bit const  value
+        )
+{
+    ASSUMPTION(0.0f <= value && value <= 1.0f);
+    layers.at(from).constants.to_layer.at(to).synapse.weight_decay_delta_per_second = value;
 }
 
 
@@ -93,8 +213,10 @@ void  cortex::next_round(float_32_bit const  round_seconds)
 {
     TMPROF_BLOCK();
 
-    update_neurons(round_seconds);
     update_existing_synapses(round_seconds);
+    update_neurons(round_seconds);
+    clear_input_signal_of_neurons();
+    disconnect_weak_synapses();
     build_new_synapses();
 }
 
@@ -103,38 +225,33 @@ void  cortex::update_neurons(float_32_bit const  round_seconds)
 {
     TMPROF_BLOCK();
 
-    // Izhikevich's differential equations are expressed in 'ms' time unit.
-    float_32_bit const  round_milli_seconds = 1000.0f * round_seconds;
+    //float_32_bit const  round_seconds = 1.0f / constants.simulation_fequency;
+    for (layer&  l : layers)
+    {
+        float_32_bit const  max_num_spikes_inv =
+                l.constants.num_dendrites_per_neuron == 0U ? 1.0f : 1.0f / l.constants.neuron.max_input_signal_magnitude;
+        //float_32_bit const  log_decay_coef = 1.0f + std::logf(l.constants.neuron.excitation_decay_coef) * round_seconds;
+        //float_32_bit const  log_decay_coef = std::logf(l.constants.neuron.excitation_decay_coef) * round_seconds;
+        float_32_bit const  log_decay_coef = l.constants.neuron.ln_of_excitation_decay_coef * round_seconds;
+        for (neuron&  n : l.neurons)
+        {
+            if (n.excitation >= n.spiking_excitation)
+                n.excitation = l.constants.neuron.excitation_recovery;
+            n.input_signal *= max_num_spikes_inv;
+            n.excitation += n.input_signal;
+            n.excitation += n.excitation * log_decay_coef;
+        }
+    }
+}
+
+
+void  cortex::clear_input_signal_of_neurons()
+{
+    TMPROF_BLOCK();
 
     for (layer&  l : layers)
         for (neuron&  n : l.neurons)
-        {
-            // Integrate Izhikevich's differential equations.
-
-            n.membrane_voltage_derivative =
-                    0.04f * n.membrane_voltage * n.membrane_voltage
-                    + 5.0f * n.membrane_voltage
-                    + 140.0f
-                    - n.recovery_voltage
-                    + n.input_current
-                    ;
-            n.recovery_voltage_derivative = 
-                    l.integration_constants.a * (l.integration_constants.b * n.membrane_voltage - n.recovery_voltage)
-                    ;
-
-            n.membrane_voltage += n.membrane_voltage_derivative * round_milli_seconds; // Euler integrator
-            n.recovery_voltage += n.recovery_voltage_derivative * round_milli_seconds; // Euler integrator
-
-            if (n.recovery_voltage > l.integration_constants.treashold)
-            {
-                n.membrane_voltage = l.integration_constants.c;
-                n.recovery_voltage += l.integration_constants.d;
-            }
-
-            // And we reset input current for the next round.
-
-            n.input_current = 0.0f;
-        }
+            n.input_signal = 0.0f;
 }
 
 
@@ -142,42 +259,67 @@ void  cortex::update_existing_synapses(float_32_bit const  round_seconds)
 {
     TMPROF_BLOCK();
 
-    // Voltage derivatives of the Izhikevich's model are expressed in 'ms' time unit.
-    float_32_bit const  round_milli_seconds = 1000.0f * round_seconds;
-
-    for (synapse_index  synapse_idx = 0U; synapse_idx < (synapse_index)synapses.size(); )
+    //float_32_bit const  round_seconds = 1.0f / constants.simulation_fequency;
+    for (synapse&  s : synapses)
     {
-        synapse&  s = synapses.at(synapse_idx);
-
         layer&  from_layer = layers.at(s.from.layer);
         neuron const&  from_neuron = from_layer.neurons.at(s.from.neuron);
 
         layer&  to_layer = layers.at(s.to.layer);
         neuron&  to_neuron = to_layer.neurons.at(s.to.neuron);
 
-        // First we deliver the presynaptic current from the 'from' neuron to the 'to' neuron.
+        // Update input signal of 'from' neuron, if 'to' neuron is spiking.
 
-        float_32_bit const  presynaptic_current_magnitude =
-                from_layer.conductances.at(s.from.layer) * from_neuron.membrane_voltage_derivative * round_milli_seconds
-                ;
-        to_neuron.input_current += from_layer.sign * presynaptic_current_magnitude * s.weight;
+        bool const  is_from_neuron_spiking = from_neuron.excitation >= from_neuron.spiking_excitation;
+        if (is_from_neuron_spiking)
+            to_neuron.input_signal += from_layer.constants.sign * s.weight;
 
-        // Now we update the weight of the synapse using the Hebbian learning algo.
+        // Now we update the weight of the synapse.
 
-        float_32_bit const  postsynaptic_current_magnitude =
-                to_layer.conductances.at(s.to.layer) * to_neuron.membrane_voltage_derivative * round_milli_seconds
-                ;
+        if (from_layer.constants.sign < 0.0f)
+        {   // inhibitory
+            layer::constant_data::to_layer_data const&  to_layer_data = from_layer.constants.to_layer.at(s.to.layer);
+            float_32_bit const  w = 2.0f * s.weight - 1.0f;
+            float_32_bit const  a =
+                    to_neuron.excitation *
+                    std::max(1.0f - w * w, 0.0001f) *
+                    to_layer_data.synapse.weight_delta_per_second;
+            s.weight = std::max(
+                            0.0f,
+                            std::min(
+                                s.weight + (a - to_layer_data.synapse.weight_decay_delta_per_second) * round_seconds,
+                                1.0f));
+        }
+        else
+        {   // excitatory
+            float_32_bit const  weight_delta = 0.0f; // TODO!
+            s.weight += weight_delta;
 
-        s.weight += (from_layer.weight_resistances.at(s.to.layer) * from_layer.sign) *
-                    (presynaptic_current_magnitude * postsynaptic_current_magnitude);
+            //float_32_bit const  postsynaptic_current_magnitude =
+            //        to_layer.conductances.at(s.to.layer) * to_neuron.membrane_voltage_derivative * round_milli_seconds
+            //        ;
 
-        // Finally, we apply the disconnection check.
+            //weight_delta = (from_layer.weight_resistances.at(s.to.layer) * from_layer.sign) *
+            //               (presynaptic_current_magnitude * postsynaptic_current_magnitude);
 
-        if (s.weight <= from_layer.disconnection_weights.at(s.to.layer))
+        }
+    }
+}
+
+
+void  cortex::disconnect_weak_synapses()
+{
+    TMPROF_BLOCK();
+
+    for (synapse_index  synapse_idx = 0U; synapse_idx < (synapse_index)synapses.size(); )
+    {
+        synapse&  s = synapses.at(synapse_idx);
+        layer&  from_layer = layers.at(s.from.layer);
+        if (s.weight <= from_layer.constants.to_layer.at(s.to.layer).synapse.weight_disconnection)
         {
             // Disconnect the synapse -> destroy it.
             from_layer.free_axons.push_back(s.from.neuron);
-            to_layer.free_dendrites.push_back(s.to.neuron);
+            layers.at(s.to.layer).free_dendrites.push_back(s.to.neuron);
             std::swap(s, synapses.back());
             synapses.pop_back();
         }
@@ -199,8 +341,9 @@ void  cortex::build_new_synapses()
             layer_index  dendrite_layer_idx = 0U;
             {
                 float_32_bit const  trial = get_random_float_32_bit_in_range(0.0f, 1.0f, random_generator);
-                while (dendrite_layer_idx + 1U < (dendrite_index)axon_layer.accumulated_connection_probabilities.size() &&
-                       trial > axon_layer.accumulated_connection_probabilities.at(dendrite_layer_idx))
+                while (dendrite_layer_idx + 1U <
+                            (dendrite_index)axon_layer.constants.to_layer.size()
+                        && trial > axon_layer.constants.to_layer.at(dendrite_layer_idx).accumulated_connection_probability)
                     ++dendrite_layer_idx;
             }
             layer&  dendrite_layer = layers.at(dendrite_layer_idx);
@@ -215,7 +358,7 @@ void  cortex::build_new_synapses()
                 synapses.push_back(synapse{
                     neuron_guid{ axon_layer_idx, axon_layer.free_axons.at(axon_idx) },                  // from
                     neuron_guid{ dendrite_layer_idx, dendrite_layer.free_dendrites.at(dendrite_idx) },  // to
-                    axon_layer.initial_weights.at(dendrite_layer_idx)                                   // weight
+                    axon_layer.constants.to_layer.at(dendrite_layer_idx).synapse.weight_initial         // weight
                     });
 
                 // Replace the axon by the last free axons in the vector.
