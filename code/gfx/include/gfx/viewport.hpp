89561a54b310683b0fd7bf1ecab67ce35f14b9d6
Marek Trtik <trtikm@gmail.com>
2021-06-05 17:07:43 +0200

    gfx: extended functionality around a viewport.

--- code/gfx/include/gfx/viewport.hpp
+++ code/gfx/include/gfx/viewport.hpp
@@ -1,13 +1,14 @@
 #ifndef GFX_VIEWPORT_HPP_INCLUDED
 #   define GFX_VIEWPORT_HPP_INCLUDED
 
-#   include <utility/basic_numeric_types.hpp>
+#   include <angeo/tensor_math.hpp>
 
 namespace gfx {
 
 
 struct  viewport
 {
+    viewport();
     viewport(
             float_32_bit const  left_,
             float_32_bit const  right_,
@@ -17,6 +18,11 @@ struct  viewport
             float_32_bit const  pixel_height_mm_
             );
 
+    bool  is_point_inside(vector2 const&  p) const;
+
+    float_32_bit  width() const { return right - left + 1.0f; }
+    float_32_bit  height() const { return top - bottom + 1.0f; }
+
     float_32_bit  left;
     float_32_bit  right;
     float_32_bit  bottom;
@@ -24,6 +30,9 @@ struct  viewport
     float_32_bit  pixel_width_mm;
     float_32_bit  pixel_height_mm;
 };
+
+
+void  make_current(viewport const&  vp);
 
 
 }
--- code/gfx/src/viewport.cpp
+++ code/gfx/src/viewport.cpp
@@ -1,7 +1,12 @@
 #include <gfx/viewport.hpp>
+#include <osi/opengl.hpp>
 #include <utility/assumptions.hpp>
 
 namespace gfx {
+
+
+viewport::viewport() : viewport(0.0f, 639.0f, 0.0f, 479.0f, 0.0001f, 0.0001f)
+{}
 
 
 viewport::viewport(
@@ -22,6 +27,18 @@ viewport::viewport(
     ASSUMPTION(left < right);
     ASSUMPTION(bottom < top);
     ASSUMPTION(pixel_width_mm > 0.0f && pixel_height_mm > 0.0f);
+}
+
+
+bool  viewport::is_point_inside(vector2 const&  p) const
+{
+    return left <= p(0) && p(0) <= right && bottom <= p(1) && p(1) <= top;
+}
+
+
+void  make_current(viewport const&  vp)
+{
+    glViewport((GLint)vp.left, (GLint)vp.bottom, (GLsizei)vp.width(), (GLsizei)vp.height());
 }
 
 
