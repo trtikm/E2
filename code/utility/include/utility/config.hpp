1807fd7e9324f79aab8aaf224ca7d17457c9b8cb
Marek Trtik <trtikm@gmail.com>
2022-01-15 22:13:40 +0100

    Added new target webassembly to cmake a utility.

--- code/utility/include/utility/config.hpp
+++ code/utility/include/utility/config.hpp
@@ -28,11 +28,14 @@ Configuration is available through pre-defined macros:
 
 #   define PLATFORM_WINDOWS()     1
 #   define PLATFORM_LINUX()       2
+#   define PLATFORM_WEBASSEMBLY() 3
 
 #   if defined(WIN32)
 #       define PLATFORM()         PLATFORM_WINDOWS()
 #   elif defined(__linux__)
 #       define PLATFORM()         PLATFORM_LINUX()
+#   elif defined(WEBASSEMBLY)
+#       define PLATFORM()         PLATFORM_WEBASSEMBLY()
 #   else
 #       error "Unsuported platform."
 #   endif
@@ -42,6 +45,8 @@ Configuration is available through pre-defined macros:
 #   define COMPILER_UNKNOWN()     0
 #   define COMPILER_VC()          1
 #   define COMPILER_GCC()         2
+#   define COMPILER_CLANG()       3
+#   define COMPILER_WEBASSEMBLY() 4
 
 #   if defined(_MSC_VER) // MSVC
 #       define COMPILER()         COMPILER_VC()
@@ -49,6 +54,12 @@ Configuration is available through pre-defined macros:
 #   elif defined(__GNUC__) // gcc
 #       define COMPILER()         COMPILER_GCC()
 #       define COMPILER_VERSION() __GNUC__.__GNUC_MINOR__.__GNUC_PATCHLEVEL__
+#   elif defined(__clang__) // CLANG
+#       define COMPILER()         COMPILER_CLANG()
+#       define COMPILER_VERSION() __clang_major__.__clang_minor__
+#   elif defined(__EMSCRIPTEN__) // WEBASSEMBLY
+#       define COMPILER()         COMPILER_WEBASSEMBLY()
+#       define COMPILER_VERSION() __EMSCRIPTEN_major__.__EMSCRIPTEN_minor__
 #   else // unknown
 #       define COMPILER()         COMPILER_UNKNOWN()
 #       define COMPILER_VERSION() 0
@@ -59,6 +70,10 @@ Configuration is available through pre-defined macros:
 #   if COMPILER() == COMPILER_VC()
 #       define FUNCTION_PROTOTYPE() __FUNCSIG__
 #   elif COMPILER() == COMPILER_GCC()
+#       define FUNCTION_PROTOTYPE() __PRETTY_FUNCTION__
+#   elif COMPILER() == COMPILER_CLANG()
+#       define FUNCTION_PROTOTYPE() __PRETTY_FUNCTION__
+#   elif COMPILER() == COMPILER_WEBASSEMBLY()
 #       define FUNCTION_PROTOTYPE() __PRETTY_FUNCTION__
 #   else // unknown => use __func__ (is guaranded by ISO standard)
 #       define FUNCTION_PROTOTYPE() __func__
